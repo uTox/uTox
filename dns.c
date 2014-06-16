bc13e695a5fee528b8c764166e54dc9387119e1e
notsecure <notsecure@marek.ca>
2014-06-16 08:05:13 -0400

    use encrypted dns3 for utox.org DNS names

--- dns.c
+++ dns.c
@@ -1,4 +1,35 @@
 #include "main.h"
+#include <tox/toxdns.h>
+
+static struct tox3
+{
+    uint8_t *name;
+    void *dns3;
+    uint8_t key[32];
+} tox3_server[] = {
+    {
+        .name = (uint8_t*)"utox.org",
+        .key = {0xD3, 0x15, 0x4F, 0x65, 0xD2, 0x8A, 0x5B, 0x41, 0xA0, 0x5D, 0x4A, 0xC7, 0xE4, 0xB3, 0x9C, 0x6B,
+        0x1C, 0x23, 0x3C, 0xC8, 0x57, 0xFB, 0x36, 0x5C, 0x56, 0xE8, 0x39, 0x27, 0x37, 0x46, 0x2A, 0x12}
+    }
+};
+
+static void* istox3(uint8_t *name, uint16_t name_length)
+{
+    int i;
+    for(i = 0; i != countof(tox3_server); i++) {
+        struct tox3 *t = &tox3_server[i];
+        if(memcmp(name, t->name, name_length) == 0 && t->name[name_length] == 0) {
+            //what if two threads reach this point at the same time?->initialize all dns3 at start instead
+            if(!t->dns3) {
+                t->dns3 = tox_dns3_new(t->key);
+            }
+            return t->dns3;
+        }
+    }
+
+    return NULL;
+}
 
 static void writechecksum(uint8_t *address)
 {
@@ -9,8 +40,10 @@ static void writechecksum(uint8_t *address)
         checksum[i % 2] ^= address[i];
 }
 
-static uint32_t parseargument(uint8_t *dest, char_t *src, uint16_t length)
+static uint32_t parseargument(uint8_t *dest, char_t *src, uint16_t length, void **pdns3)
 {
+    /* parses format groupbot@utox.org -> groupbot._tox.utox.org */
+
     _Bool reset = 0, at = 0;
     char_t *a = src, *b = src + length;
     uint8_t *d = dest;
@@ -67,6 +100,18 @@ static uint32_t parseargument(uint8_t *dest, char_t *src, uint16_t length)
         }
 
         case '@': {
+            void *dns3;
+            if((dns3 = istox3(a + 1, b - (a + 1)))) {
+                uint8_t out[256];
+                int len = tox_generate_dns3_string(dns3, out, sizeof(out), &pin, dest, d - dest);
+                if(len != -1) {
+                    dest[0] = '_';
+                    memcpy(dest + 1, out, len);
+                    d = dest + 1 + len;
+                    *pdns3 = dns3;
+                }
+            }
+
             memcpy(d, "._tox.", 6);
             d += 6;
             at = 1;
@@ -82,7 +127,7 @@ static uint32_t parseargument(uint8_t *dest, char_t *src, uint16_t length)
     }
 
     if(!at) {
-        memcpy(d, "._tox.toxme.se", 14);
+        memcpy(d, "._tox.utox.org", 14);
         d += 14;
     }
 
@@ -93,7 +138,7 @@ static uint32_t parseargument(uint8_t *dest, char_t *src, uint16_t length)
     return pin;
 }
 
-static _Bool parserecord(uint8_t *dest, uint8_t *src, uint32_t pin)
+static _Bool parserecord(uint8_t *dest, uint8_t *src, uint32_t pin, void *dns3)
 {
     _Bool _id = 0, _pub = 0, b = 0;
     uint32_t version = 0;
@@ -169,9 +214,15 @@ static _Bool parserecord(uint8_t *dest, uint8_t *src, uint32_t pin)
             }
         }
 
+
         if(version == 1 && dest == id && memcmp("id=", a, 3) == 0) {
             _id = 1;
             a += 2;
+        }
+
+        if(version == 3 && dest == id && memcmp("id=", a, 3) == 0) {
+            a += 3;
+            return (tox_decrypt_dns3_TXT(dns3, id, a, strlen((char*)a), pin) == 0);
         }
 
         if(version == 2 && dest == id && memcmp("pub=", a, 4) == 0) {
@@ -179,12 +230,11 @@ static _Bool parserecord(uint8_t *dest, uint8_t *src, uint32_t pin)
             a += 3;
         }
 
-        if(!version && memcmp("v=tox1", a, 6) == 0) {
-            version = 1;
-        }
-
-        if(!version && memcmp("v=tox2", a, 6) == 0) {
-            version = 2;
+        if(!version && memcmp("v=tox", a, 5) == 0) {
+            if(a[5] >= '1' && a[5] <= '3') {
+                version = a[5] - '0';
+                a += 5;
+            }
         }
 
         a++;
@@ -203,7 +253,8 @@ static void dns_thread(void *data)
     uint16_t length = *(uint16_t*)data;
     uint8_t result[256];
 
-    uint32_t pin = parseargument(result, data + 2, length);
+    void *dns3 = NULL;
+    uint32_t pin = parseargument(result, data + 2, length, &dns3);
     _Bool success = 0;
 
     #ifdef __WIN32__
@@ -214,7 +265,7 @@ static void dns_thread(void *data)
         DNS_TXT_DATA *txt = &record->Data.Txt;
         if(txt->pStringArray[0]) {
             debug("Attempting:\n%s\n", txt->pStringArray[0]);
-            if((success = parserecord(data, (uint8_t*)txt->pStringArray[0], pin))) {
+            if((success = parserecord(data, (uint8_t*)txt->pStringArray[0], pin, dns3))) {
                 break;
             }
         }
@@ -294,7 +345,7 @@ static void dns_thread(void *data)
         debug("Attempting:\n%.*s\n", txtlen, pt + 1);
 
         pt[txtlen + 1] = 0;
-        success = parserecord(data, pt + 1, pin);
+        success = parserecord(data, pt + 1, pin, dns3);
     } else {
         debug("timeout\n");
     }
