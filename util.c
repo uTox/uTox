0b47be1a96bce3032b109c679b6adf204faa0d88
irungentoo <irungentoo@gmail.com>
2014-09-20 13:01:57 -0400

    Fixed uTox crash when printing 1TiB+ file sizes.

--- util.c
+++ util.c
@@ -162,7 +162,7 @@ _Bool string_to_id(char_t *w, char_t *a)
 
 int sprint_bytes(uint8_t *dest, uint64_t bytes)
 {
-    char *str[] = {"B", "KiB", "MiB", "GiB"};
+    char *str[] = {"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"};
     int i = 0;
     double f = bytes;
     while(bytes >= 1024) {
