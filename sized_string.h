35daae14c0bf8bf80dc86c6f9cd3d1a82a57a5a0
Alexander Belykh <albel727@ngs.ru>
2014-09-25 20:20:44 +0700

    Introduce STRING_IDX typedef for STRING index/size use

--- sized_string.h
+++ sized_string.h
@@ -1,9 +1,12 @@
 
 typedef uint8_t char_t;
+// Unsigned integer type for string indices and sizes.
+typedef uint16_t STRING_IDX;
+#define STRING_IDX_MAX (UINT16_MAX)
 
 typedef struct {
     char_t *str;
-    uint16_t length;
+    STRING_IDX length;
 } STRING;
 
 #define STRING_INIT(x) { .str = (char_t*)x, .length = sizeof(x) - 1 }
