7493fc714c2c3e2551db9e7e490f1381acb11438
banditkroot <bandit.kroot@gmail.com>
2014-09-29 14:39:56 +0200

    Update french translation.

--- langs/fr.h
+++ langs/fr.h
@@ -208,19 +208,8 @@ msgstr("Aucun")
 msgid(VIDEO_IN_DESKTOP)
 msgstr("Ecran")
 
-//TODO: Localize or remove.
-//msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
-//msgstr("Please accept this friend request.")
+msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
+msgstr("Veuillez accepter cette requête d'ami.")
 
-//TODO: Localize or remove.
-//msgid(CONTACTS_FILTER_EDIT_HINT)
-//msgstr("Search friends")
-
-//TODO: Localize or remove.
-//msgid(PROXY_EDIT_HINT_IP)
-//msgstr("IP")
-
-//TODO: Localize or remove.
-//msgid(PROXY_EDIT_HINT_PORT)
-//msgstr("Port")
-
+msgid(CONTACTS_FILTER_EDIT_HINT)
+msgstr("Recherche d'ami")
