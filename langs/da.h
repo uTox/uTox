37f6c149880c101262b6b95e2f32e180b918aaab
Nick Bakkegaard <nickbakkegaard@gmail.com>
2014-10-14 17:32:20 +0200

    added danish translation

---
+++ langs/da.h
@@ -0,0 +1,252 @@
+msgid(LANG_NATIVE_NAME)
+msgstr("Dansk")
+
+msgid(LANG_ENGLISH_NAME)
+msgstr("DANISH")
+
+msgid(REQ_SENT)
+msgstr("Venneanmodning sendt. Din ven vil forekomme online når anmodningen er accepteret.")
+
+msgid(REQ_RESOLVE)
+msgstr("Prøver at afkode DNS navn...")
+
+msgid(REQ_INVALID_ID)
+msgstr("Fejl: Ugyldigt Tox ID")
+
+msgid(REQ_EMPTY_ID)
+msgstr("Fejl: Intet Tox ID specificeret")
+
+msgid(REQ_LONG_MSG)
+msgstr("Fejl: Besked er for lang")
+
+msgid(REQ_NO_MSG)
+msgstr("Fejl: Tom besked")
+
+msgid(REQ_SELF_ID)
+msgstr("Fejl: Tox ID er eget ID")
+
+msgid(REQ_ALREADY_FRIENDS)
+msgstr("Fejl: Tox ID er allerede på vennelisten")
+
+msgid(REQ_UNKNOWN)
+msgstr("Fejl: Ukendt")
+
+msgid(REQ_BAD_CHECKSUM)
+msgstr("Fejl: Ugyldigt Tox ID (forkert checksum)")
+
+msgid(REQ_BAD_NOSPAM)
+msgstr("Fejl: Ugyldigt Tox ID (forkert nospam værdi)")
+
+msgid(REQ_NO_MEMORY)
+msgstr("Fejl: Ingen hukommelse")
+
+msgid(TRANSFER_NEW)
+msgstr("Ny filoverførsel")
+
+msgid(TRANSFER_STARTED)
+msgstr("Filoverførsel startet")
+
+//msgid(TRANSFER___)
+//msgstr("...")
+
+msgid(TRANSFER_PAUSED)
+msgstr("Filoverførsel sat på pause")
+
+msgid(TRANSFER_BROKEN)
+msgstr("Filoverførsel fejlede")
+
+msgid(TRANSFER_CANCELLED)
+msgstr("Filoverførsel annulleret")
+
+msgid(TRANSFER_COMPLETE)
+msgstr("Filoverførsel færdig")
+
+msgid(CALL_CANCELLED)
+msgstr("Opkald annulleret")
+
+msgid(CALL_INVITED)
+msgstr("Opkald inviteret")
+
+msgid(CALL_RINGING)
+msgstr("Opkald ringer")
+
+msgid(CALL_STARTED)
+msgstr("Opkald startet")
+
+msgid(ADDFRIENDS)
+msgstr("Tilføj venner")
+
+//msgid(TOXID)
+//msgstr("Tox ID")
+
+msgid(MESSAGE)
+msgstr("Besked")
+
+msgid(SEARCHFRIENDS)
+msgstr("Søg i venner")
+
+msgid(ADD)
+msgstr("Tilføj")
+
+msgid(SWITCHPROFILE)
+msgstr("Skift profil")
+
+msgid(FRIENDREQUEST)
+msgstr("Venneanmodning")
+
+msgid(USERSETTINGS)
+msgstr("Brugerindstillinger")
+
+msgid(NAME)
+msgstr("Navn")
+
+msgid(STATUSMESSAGE)
+msgstr("Status besked")
+
+msgid(PREVIEW)
+msgstr("Eksempel")
+
+msgid(DEVICESELECTION)
+msgstr("Vælg enhed")
+
+msgid(AUDIOINPUTDEVICE)
+msgstr("Lyd input enhed")
+
+msgid(AUDIOOUTPUTDEVICE)
+msgstr("Lyd output enhed")
+
+msgid(VIDEOINPUTDEVICE)
+msgstr("Video output enhed")
+
+msgid(OTHERSETTINGS)
+msgstr("Andre indstillinger")
+
+//msgid(DPI)
+//msgstr("DPI")
+
+msgid(SAVELOCATION)
+msgstr("Gem område")
+
+msgid(LANGUAGE)
+msgstr("Sprog")
+
+msgid(NETWORK)
+msgstr("Netværk")
+
+//msgid(IPV6)
+//msgstr("IPv6:")
+
+//msgid(UDP)
+//msgstr("UDP:")
+
+//msgid(PROXY)
+//msgstr("Proxy (SOCKS 5)")
+
+msgid(WARNING)
+msgstr("Ændring af Netværk/Proxy indstillinger vil midlertidigt afbryde din forbindelse til Tox netværket")
+
+msgid(LOGGING)
+msgstr("Logger")
+
+msgid(IS_TYPING)
+msgstr("skriver...")
+
+msgid(COPY)
+msgstr("Kopier")
+
+msgid(COPYWITHOUTNAMES)
+msgstr("Kopier (ingen navne)")
+
+msgid(CUT)
+msgstr("Klip")
+
+msgid(PASTE)
+msgstr("Indsæt")
+
+msgid(DELETE)
+msgstr("Slet")
+
+msgid(SELECTALL)
+msgstr("Vælg alle")
+
+msgid(REMOVE)
+msgstr("Fjern")
+
+msgid(LEAVE)
+msgstr("Forlad")
+
+msgid(ACCEPT)
+msgstr("Accepter")
+
+msgid(IGNORE)
+msgstr("Ignorer")
+
+msgid(CLICKTOSAVE)
+msgstr("Klik for at gemme")
+
+msgid(CLICKTOOPEN)
+msgstr("Klik for at åbne")
+
+msgid(CANCELLED)
+msgstr("Annulleret")
+
+msgid(DPI_TINY)
+msgstr("Lille (50%)")
+
+//msgid(DPI_NORMAL)
+//msgstr("Normal (100%)")
+
+msgid(DPI_BIG)
+msgstr("Stor (150%)")
+
+msgid(DPI_LARGE)
+msgstr("Kæmpe (200%)")
+
+msgid(DPI_HUGE)
+msgstr("Enorm (250%)")
+
+msgid(CONTACTS_FILTER_ALL)
+msgstr("Alle")
+
+//msgid(CONTACTS_FILTER_ONLINE)
+//msgstr("Online")
+
+msgid(PROXY_DISABLED)
+msgstr("Ikke slået til")
+
+//msgid(PROXY_FALLBACK)
+//msgstr("Fallback")
+
+msgid(PROXY_ALWAYS_USE)
+msgstr("Brug altid")
+
+msgid(NO)
+msgstr("Nej")
+
+msgid(YES)
+msgstr("Ja")
+
+msgid(VIDEO_IN_NONE)
+msgstr("Ingen")
+
+msgid(VIDEO_IN_DESKTOP)
+msgstr("Skrivebord")
+
+msgid(AUDIO_IN_DEFAULT_LOOPBACK)
+msgstr("Standard Loopback")
+
+msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
+msgstr("Accepter venligst denne venneanmodning.")
+
+msgid(CONTACTS_FILTER_EDIT_HINT)
+msgstr("Find venner")
+
+//msgid(PROXY_EDIT_HINT_IP)
+//msgstr("IP")
+
+//msgid(PROXY_EDIT_HINT_PORT)
+//msgstr("Port")
+
+msgid(WINDOW_TITLE_VIDEO_PREVIEW)
+msgstr("Video prøve")
+
