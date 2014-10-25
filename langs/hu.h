521862fa5b312c225064d091d78baaf7177acaab
baloo79 <baloo79@users.noreply.github.com>
2014-10-25 20:51:46 +0200

    Hungarian language added

---
+++ langs/hu.h
@@ -0,0 +1,255 @@
+msgid(LANG_NATIVE_NAME)
+msgstr("Magyar")
+
+msgid(LANG_ENGLISH_NAME)
+msgstr("HUNGARIAN")
+
+msgid(REQ_SENT)
+msgstr("Partnerkérelem elküldve. A partner elérhető lesz, miután a kérelmet elf$
+
+msgid(REQ_RESOLVE)
+msgstr("DNS név feloldásának kísérlete...")
+
+msgid(REQ_INVALID_ID)
+msgstr("Hiba: Érvénytelen Tox ID")
+
+msgid(REQ_EMPTY_ID)
+msgstr("Hiba: Nincs megadva Tox ID")
+
+msgid(REQ_LONG_MSG)
+msgstr("Hiba: Az üzenet túl hosszú")
+
+msgid(REQ_NO_MSG)
+msgstr("Hiba: Üres üzenet")
+
+msgid(REQ_SELF_ID)
+msgstr("Hiba: A Tox ID ugyanaz")
+
+msgid(REQ_ALREADY_FRIENDS)
+msgstr("Hiba: A Tox ID már a partnerlistában van")
+
+msgid(REQ_UNKNOWN)
+msgstr("Hiba: Ismeretlen")
+
+msgid(REQ_BAD_CHECKSUM)
+msgstr("Hiba: Érvénytelen Tox ID (hibás checksum)")
+
+msgid(REQ_BAD_NOSPAM)
+msgstr("Hiba: Érvénytelen Tox ID (hibás nospam érték)")
+
+msgid(REQ_NO_MEMORY)
+msgstr("Hiba: Nincs memória")
+
+msgid(TRANSFER_NEW)
+msgstr("Új fájl átvitel")
+
+msgid(TRANSFER_STARTED)
+msgstr("Fájl átvitel elindult")
+
+msgid(TRANSFER___)
+msgstr("...")
+
+msgid(TRANSFER_PAUSED)
+msgstr("Fájl átvitel szünetelve")
+
+msgid(TRANSFER_BROKEN)
+msgstr("Fájl átvitel sikertelen")
+
+msgid(TRANSFER_CANCELLED)
+msgstr("Fájl átvitel megszakadt")
+
+msgid(TRANSFER_COMPLETE)
+msgstr("Fájl átvitel sikeres")
+
+msgid(CALL_CANCELLED)
+msgstr("Hívás megszakadt")
+
+msgid(CALL_INVITED)
+msgstr("Hívás fogadva")
+
+msgid(CALL_RINGING)
+msgstr("Hívás")
+
+msgid(CALL_STARTED)
+msgstr("Hívás elindult")
+
+msgid(ADDFRIENDS)
+msgstr("Partner hozzáadása")
+
+msgid(TOXID)
+msgstr("Tox ID")
+
+msgid(MESSAGE)
+msgstr("Üzenet")
+
+msgid(SEARCHFRIENDS)
+msgstr("Partnerek keresése")
+
+msgid(ADD)
+msgstr("Hozzáadás")
+
+msgid(SWITCHPROFILE)
+msgstr("Profilváltás")
+
+msgid(FRIENDREQUEST)
+msgstr("Partnerkérelem")
+
+msgid(USERSETTINGS)
+msgstr("Beállítások")
+
+msgid(NAME)
+msgstr("Név")
+
+msgid(STATUSMESSAGE)
+msgstr("Állapot üzenet")
+
+msgid(PREVIEW)
+msgstr("Előnézet")
+
+msgid(DEVICESELECTION)
+msgstr("Eszközválasztás")
+
+msgid(AUDIOINPUTDEVICE)
+msgstr("Bemeneti hangeszköz")
+
+msgid(AUDIOOUTPUTDEVICE)
+msgstr("Kimeneti hangeszköz")
+
+msgid(VIDEOINPUTDEVICE)
+msgstr("Bemeneti videoeszköz")
+
+msgid(OTHERSETTINGS)
+msgstr("Egyéb beállítások")
+
+msgid(DPI)
+msgstr("DPI")
+
+msgid(SAVELOCATION)
+msgstr("Mentés helye")
+
+msgid(LANGUAGE)
+msgstr("Nyelv")
+
+msgid(NETWORK)
+msgstr("Hálózat")
+
+msgid(IPV6)
+msgstr("IPv6:")
+
+msgid(UDP)
+msgstr("UDP:")
+
+msgid(PROXY)
+msgstr("Proxy (SOCKS 5)")
+
+msgid(WARNING)
+msgstr("A hálózati-, vagy proxybeállítások megváltoztatása miatt Ön átmenetileg nem lesz elérhető a Tox hálózatán")
+
+msgid(LOGGING)
+msgstr("Naplózás")
+
+msgid(AUDIONOTIFICATIONS)
+msgstr("Hallható jelzés (csengőhang) engedélyezése")
+
+msgid(IS_TYPING)
+msgstr("éppen ír...")
+
+msgid(COPY)
+msgstr("Másolás")
+
+msgid(COPYWITHOUTNAMES)
+msgstr("Másolás (nincs név)")
+
+msgid(CUT)
+msgstr("Kivágás")
+
+msgid(PASTE)
+msgstr("Beillesztés")
+
+msgid(DELETE)
+msgstr("Törlés")
+
+msgid(SELECTALL)
+msgstr("Mindet kiválaszt")
+
+msgid(REMOVE)
+msgstr("Eltávolítás")
+
+msgid(LEAVE)
+msgstr("Kihagyás")
+
+msgid(ACCEPT)
+msgstr("Elfogadás")
+
+msgid(IGNORE)
+msgstr("Mellőzés")
+
+msgid(CLICKTOSAVE)
+msgstr("Kattintson a mentéshez")
+
+msgid(CLICKTOOPEN)
+msgstr("Kattintson a megnyitáshoz")
+
+msgid(CANCELLED)
+msgstr("Megszakítva")
+
+msgid(DPI_TINY)
+msgstr("Kicsi (50%)")
+
+msgid(DPI_NORMAL)
+msgstr("Normál (100%)")
+
+msgid(DPI_BIG)
+msgstr("Nagy (150%)")
+
+msgid(DPI_LARGE)
+msgstr("Nagyobb (200%)")
+
+msgid(DPI_HUGE)
+msgstr("Legnagyobb (250%)")
+
+msgid(CONTACTS_FILTER_ALL)
+msgstr("Összes")
+
+msgid(CONTACTS_FILTER_ONLINE)
+msgstr("Elérhető")
+
+msgid(PROXY_DISABLED)
+msgstr("Letiltva")
+
+msgid(PROXY_FALLBACK)
+msgstr("Fallback")
+
+msgid(PROXY_ALWAYS_USE)
+msgstr("Mindig használja")
+
+msgid(NO)
+msgstr("Nem")
+
+msgid(YES)
+msgstr("Igen")
+
+msgid(VIDEO_IN_NONE)
+msgstr("Nincs")
+
+msgid(VIDEO_IN_DESKTOP)
+msgstr("Asztal")
+
+msgid(AUDIO_IN_DEFAULT_LOOPBACK)
+msgstr("Alapértelmezett Loopback")
+
+msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
+msgstr("Kérem, fogadja el ezt a partnerkérelmet!")
+
+msgid(CONTACTS_FILTER_EDIT_HINT)
+msgstr("Partnerek keresése")
+
+msgid(PROXY_EDIT_HINT_IP)
+msgstr("IP")
+
+msgid(PROXY_EDIT_HINT_PORT)
+msgstr("Port")
+
+msgid(WINDOW_TITLE_VIDEO_PREVIEW)
+msgstr("Videó előnézet")
+
