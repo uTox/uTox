95d222facada4ba7e24f6678dcedc80afddcae01
flan <flan@flande.re>
2015-03-06 19:07:33 +0100

    fix text not visible on mouseover in highcontrast

--- theme.c
+++ theme.c
@@ -136,7 +136,7 @@ void theme_load(char loadtheme)
         COLOR_MENU_ACTIVE_BACKGROUND          = COLOR_MAIN_TEXT;
         COLOR_MENU_ACTIVE_TEXT                = COLOR_MAIN_BACKGROUND;
         COLOR_LIST_BACKGROUND                 = COLOR_PROC(0x444444);
-        COLOR_LIST_HOVER_BACKGROUND           = COLOR_MAIN_TEXT;
+        COLOR_LIST_HOVER_BACKGROUND           = COLOR_MAIN_BACKGROUND;
         COLOR_LIST_TEXT                       = COLOR_MAIN_TEXT;
         COLOR_LIST_SUBTEXT                    = COLOR_MAIN_TEXT;
         COLOR_SELECTION_BACKGROUND            = COLOR_MAIN_TEXT;
