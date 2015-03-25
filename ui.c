6c6df4bc945a93516d92af8f66ed5efe98f7de3c
Mate Bene <mat.test07@gmail.com>
2015-03-25 23:19:26 +0100

    Auto startup feature implemented.

--- ui.c
+++ ui.c
@@ -302,7 +302,9 @@ static void drawsettings_content(int UNUSED(x), int y, int UNUSED(w), int UNUSED
     drawstr(LIST_RIGHT + SCALE * 5, y + SCALE * 357, CLOSE_TO_TRAY);
     drawstr(LIST_RIGHT + SCALE * 95, y + SCALE * 357, START_IN_TRAY);
 
-    drawstr(LIST_RIGHT + SCALE * 5, y + SCALE * 380, SEND_TYPING_NOTIFICATIONS);
+    drawstr(LIST_RIGHT + SCALE * 5, y + SCALE * 380, AUTO_STARTUP);
+
+    drawstr(LIST_RIGHT + SCALE * 5, y + SCALE * 403, SEND_TYPING_NOTIFICATIONS);
 }
 
 static void background_draw(PANEL *UNUSED(p), int UNUSED(x), int UNUSED(y), int width, int height)
@@ -429,6 +431,7 @@ panel_settings = {
         (void*)&dropdown_audible_notification, (void*)&dropdown_audio_filtering,
         (void*)&dropdown_close_to_tray, (void*)&dropdown_start_in_tray,
         (void*)&dropdown_theme,
+        (void*)&dropdown_auto_startup,
         (void*)&dropdown_typing_notes,
         NULL
     }
@@ -554,7 +557,7 @@ void ui_scale(uint8_t scale)
     messages_group.panel.width = -SCROLL_WIDTH;
 
     scroll_settings.panel.y = LIST_Y;
-    scroll_settings.content_height = 414 * SCALE;
+    scroll_settings.content_height = 425 * SCALE;
 
     scroll_group.panel.y = LIST_Y;
     scroll_group.panel.height = MESSAGES_BOTTOM;
@@ -841,6 +844,14 @@ void ui_scale(uint8_t scale)
         .width = SCALE * 20
     },
 
+    d_theme = {
+        .type = PANEL_DROPDOWN,
+        .x = 95 * SCALE,
+        .y = SCALE * 320,
+        .height = SCALE * 12,
+        .width = SCALE * 60
+    },
+
     d_notifications = {
         .type = PANEL_DROPDOWN,
         .x = 5 * SCALE,
@@ -865,7 +876,7 @@ void ui_scale(uint8_t scale)
         .width = SCALE * 20
     },
 
-    d_typing_notes = {
+    d_auto_startup = {
         .type = PANEL_DROPDOWN,
         .x = 5 * SCALE,
         .y = SCALE * 389,
@@ -873,12 +884,12 @@ void ui_scale(uint8_t scale)
         .width = SCALE * 20
     },
 
-    d_theme = {
+    d_typing_notes = {
         .type = PANEL_DROPDOWN,
-        .x = 95 * SCALE,
-        .y = SCALE * 320,
+        .x = 5 * SCALE,
+        .y = SCALE * 412,
         .height = SCALE * 12,
-        .width = SCALE * 60
+        .width = SCALE * 20
     }
 
 #ifdef AUDIO_FILTERING
@@ -906,6 +917,8 @@ void ui_scale(uint8_t scale)
     dropdown_close_to_tray.panel = d_close_to_tray;
     dropdown_start_in_tray.panel = d_start_in_tray;
     dropdown_theme.panel = d_theme;
+    dropdown_auto_startup.panel = d_auto_startup;
+
 #ifdef AUDIO_FILTERING
     dropdown_audio_filtering.panel = d_audio_filtering;
 #endif
