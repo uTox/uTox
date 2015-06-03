f8981acff96462bbb7583649657e06fd20f2c271
GrimKriegor <grimkriegor@gmail.com>
2015-06-03 16:21:38 +0100

    Beginning PT_pt localization.

---
+++ langs/pt.h
@@ -0,0 +1,338 @@
+msgid(LANG_NATIVE_NAME)
+msgstr("English")
+
+msgid(LANG_ENGLISH_NAME)
+msgstr("ENGLISH")
+
+msgid(REQ_SENT)
+msgstr("Friend request sent. Your friend will appear online after the request is accepted.")
+
+msgid(REQ_RESOLVE)
+msgstr("Attempting to resolve DNS name...")
+
+msgid(REQ_INVALID_ID)
+msgstr("Error: Invalid Tox ID")
+
+msgid(REQ_EMPTY_ID)
+msgstr("Error: No Tox ID specified")
+
+msgid(REQ_LONG_MSG)
+msgstr("Error: Message is too long")
+
+msgid(REQ_NO_MSG)
+msgstr("Error: Empty message")
+
+msgid(REQ_SELF_ID)
+msgstr("Error: Tox ID is self ID")
+
+msgid(REQ_ALREADY_FRIENDS)
+msgstr("Error: Tox ID is already in friend list")
+
+msgid(REQ_UNKNOWN)
+msgstr("Error: Unknown")
+
+msgid(REQ_BAD_CHECKSUM)
+msgstr("Error: Invalid Tox ID (bad checksum)")
+
+msgid(REQ_BAD_NOSPAM)
+msgstr("Error: Invalid Tox ID (bad nospam value)")
+
+msgid(REQ_NO_MEMORY)
+msgstr("Error: No memory")
+
+msgid(SEND_FILE)
+msgstr("Send File")
+
+msgid(SAVE_FILE)
+msgstr("Save File")
+
+msgid(WHERE_TO_SAVE_FILE_PROMPT)
+msgstr("Where do you want to save \"%.*s\"?")
+
+msgid(SEND_FILE_PROMPT)
+msgstr("Select one or more files to send.")
+
+msgid(SCREEN_CAPTURE_PROMPT)
+msgstr("Drag a box around the area of the screen you want to send.")
+
+msgid(TRANSFER_NEW)
+msgstr("New file transfer")
+
+msgid(TRANSFER_STARTED)
+msgstr("File transfer started")
+
+msgid(TRANSFER___)
+msgstr("...")
+
+msgid(TRANSFER_PAUSED)
+msgstr("File transfer paused")
+
+msgid(TRANSFER_BROKEN)
+msgstr("File transfer broken")
+
+msgid(TRANSFER_CANCELLED)
+msgstr("File transfer canceled")
+
+msgid(TRANSFER_COMPLETE)
+msgstr("File transfer complete")
+
+msgid(CALL_CANCELLED)
+msgstr("Call canceled")
+
+msgid(CALL_INVITED)
+msgstr("Call invited")
+
+msgid(CALL_RINGING)
+msgstr("Call ringing")
+
+msgid(CALL_STARTED)
+msgstr("Call started")
+
+msgid(ADDFRIENDS)
+msgstr("Add Friends")
+
+msgid(TOXID)
+msgstr("Tox ID")
+
+msgid(MESSAGE)
+msgstr("Message")
+
+msgid(SEARCHFRIENDS)
+msgstr("Search friends")
+
+msgid(ADD)
+msgstr("Add")
+
+msgid(CREATEGROUPCHAT)
+msgstr("Create Groupchat")
+
+msgid(SWITCHPROFILE)
+msgstr("Switch Profile")
+
+msgid(FRIENDREQUEST)
+msgstr("Friend Request")
+
+msgid(USERSETTINGS)
+msgstr("User Settings")
+
+msgid(NAME)
+msgstr("Name")
+
+msgid(STATUSMESSAGE)
+msgstr("Status Message")
+
+msgid(PREVIEW)
+msgstr("Preview")
+
+msgid(DEVICESELECTION)
+msgstr("Device Selection")
+
+msgid(AUDIOINPUTDEVICE)
+msgstr("Audio Input Device")
+
+msgid(AUDIOFILTERING)
+msgstr("Filter Audio")
+
+msgid(AUDIOOUTPUTDEVICE)
+msgstr("Audio Output Device")
+
+msgid(VIDEOINPUTDEVICE)
+msgstr("Video Input Device")
+
+msgid(OTHERSETTINGS)
+msgstr("Other Settings")
+
+msgid(DPI)
+msgstr("DPI")
+
+msgid(SAVELOCATION)
+msgstr("Save Location")
+
+msgid(LANGUAGE)
+msgstr("Language")
+
+msgid(NETWORK)
+msgstr("Network")
+
+msgid(IPV6)
+msgstr("IPv6:")
+
+msgid(UDP)
+msgstr("UDP:")
+
+msgid(PROXY)
+msgstr("Proxy (SOCKS 5)")
+
+msgid(WARNING)
+msgstr("Changing Network/Proxy settings will temporarily disconnect you from the Tox network")
+
+msgid(LOGGING)
+msgstr("Logging")
+
+msgid(AUDIONOTIFICATIONS)
+msgstr("Enable Audible Notification (Ringtone)")
+
+msgid(IS_TYPING)
+msgstr("is typing...")
+
+msgid(CLOSE_TO_TRAY)
+msgstr("Close To Tray")
+
+msgid(START_IN_TRAY)
+msgstr("Start In Tray")
+
+msgid(COPY)
+msgstr("Copy")
+
+msgid(COPYWITHOUTNAMES)
+msgstr("Copy (Without Names)")
+
+msgid(COPY_WITH_NAMES)
+msgstr("Copy (Include Names)")
+
+msgid(CUT)
+msgstr("Cut")
+
+msgid(PASTE)
+msgstr("Paste")
+
+msgid(DELETE)
+msgstr("Delete")
+
+msgid(SELECTALL)
+msgstr("Select All")
+
+msgid(REMOVE)
+msgstr("Remove")
+
+msgid(LEAVE)
+msgstr("Leave")
+
+msgid(CTOPIC)
+msgstr("Change topic")
+
+msgid(ACCEPT)
+msgstr("Accept")
+
+msgid(IGNORE)
+msgstr("Ignore")
+
+msgid(SENDSCREENSHOT)
+msgstr("Send a screenshot")
+
+msgid(CLICKTOSAVE)
+msgstr("Click to save")
+
+msgid(CLICKTOOPEN)
+msgstr("Click to open")
+
+msgid(CANCELLED)
+msgstr("Cancelled")
+
+msgid(DPI_TINY)
+msgstr("Tiny (50%)")
+
+msgid(DPI_NORMAL)
+msgstr("Normal (100%)")
+
+msgid(DPI_BIG)
+msgstr("Big (150%)")
+
+msgid(DPI_LARGE)
+msgstr("Large (200%)")
+
+msgid(DPI_HUGE)
+msgstr("Huge (250%)")
+
+msgid(CONTACTS_FILTER_ALL)
+msgstr("All")
+
+msgid(CONTACTS_FILTER_ONLINE)
+msgstr("Online")
+
+msgid(PROXY_DISABLED)
+msgstr("Disabled")
+
+msgid(PROXY_FALLBACK)
+msgstr("Fallback")
+
+msgid(PROXY_ALWAYS_USE)
+msgstr("Always use")
+
+msgid(NO)
+msgstr("No")
+
+msgid(YES)
+msgstr("Yes")
+
+msgid(VIDEO_IN_NONE)
+msgstr("None")
+
+msgid(VIDEO_IN_DESKTOP)
+msgstr("Desktop")
+
+msgid(AUDIO_IN_DEFAULT_LOOPBACK)
+msgstr("Default Loopback")
+
+msgid(AUDIO_IN_ANDROID)
+msgstr("OpenSL Input")
+
+msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
+msgstr("Please accept this friend request.")
+
+msgid(CONTACTS_FILTER_EDIT_HINT)
+msgstr("Search friends")
+
+msgid(PROXY_EDIT_HINT_IP)
+msgstr("IP")
+
+msgid(PROXY_EDIT_HINT_PORT)
+msgstr("Port")
+
+msgid(WINDOW_TITLE_VIDEO_PREVIEW)
+msgstr("Video Preview")
+
+msgid(MUTE)
+msgstr("Mute")
+
+msgid(UNMUTE)
+msgstr("Unmute")
+
+msgid(SELECT_AVATAR_TITLE)
+msgstr("Select Avatar")
+
+msgid(AVATAR_TOO_LARGE_MAX_SIZE_IS)
+msgstr("Avatar too large. Maximum size: ")
+
+msgid(CANT_FIND_FILE_OR_EMPTY)
+msgstr("Cannot find selected file or selected file is empty.")
+
+msgid(CLEAR_HISTORY)
+msgstr("Clear history")
+
+msgid(AUTO_STARTUP)
+msgstr("Launch at system startup")
+
+msgid(THEME)
+msgstr("Theme")
+
+msgid(THEME_DEFAULT)
+msgstr("Default")
+
+msgid(THEME_LIGHT)
+msgstr("Light theme")
+
+msgid(THEME_DARK)
+msgstr("Dark theme")
+
+msgid(THEME_HIGHCONTRAST)
+msgstr("High contrast")
+
+msgid(THEME_CUSTOM)
+msgstr("Custom (see docs)")
+
+msgid(THEME_ZENBURN)
+msgstr("Zenburn")
+
+msgid(SEND_TYPING_NOTIFICATIONS)
+msgstr("Send typing notifications")
