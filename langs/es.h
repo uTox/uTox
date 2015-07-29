47c1850ad5a6faef0376c44cf84ffabca9a33107
Gran PC <gran.pc@gmail.com>
2015-07-29 20:40:55 +0200

    Rewrote Spanish translations
    They were pretty bad.

--- langs/es.h
+++ langs/es.h
@@ -5,52 +5,70 @@ msgid(LANG_ENGLISH_NAME)
 msgstr("SPANISH")
 
 msgid(REQ_SENT)
-msgstr("Solicitud de Amistad enviada. Esta persona aparecerá conectada cuando acepte la solicitud.")
+msgstr("Solicitud de amistad enviada. Esta persona aparecerá conectada cuando acepte la solicitud.")
 
 msgid(REQ_RESOLVE)
-msgstr("Resolviendo DNS...")
+msgstr("Intentando resolver nombre DNS...")
 
 msgid(REQ_INVALID_ID)
-msgstr("Error: Tox ID invalida")
+msgstr("Error: ID de Tox inválida")
 
 msgid(REQ_EMPTY_ID)
-msgstr("Error: Ninguna Tox ID especificada")
+msgstr("Error: ID de Tox no especificada")
 
 msgid(REQ_LONG_MSG)
-msgstr("Error: Mensaje demasiado largo")
+msgstr("Error: El mensaje es demasiado largo")
 
 msgid(REQ_NO_MSG)
-msgstr("Error: Mensaje vació")
+msgstr("Error: Mensaje vacío")
 
 msgid(REQ_SELF_ID)
-msgstr("Error: Tox ID es ID propia")
+msgstr("Error: La ID de Tox es tu ID propia")
 
 msgid(REQ_ALREADY_FRIENDS)
-msgstr("Error: Tox ID ya se encuentra en la lista de amigos")
+msgstr("Error: La ID de Tox ya está en la lista de amigos")
 
 msgid(REQ_UNKNOWN)
 msgstr("Error: Desconocido")
 
 msgid(REQ_BAD_CHECKSUM)
-msgstr("Error: Tox ID Invalida (Checksum Incorrecta)")
+msgstr("Error: ID de Tox inválida (checksum incorrecto)")
 
 msgid(REQ_BAD_NOSPAM)
-msgstr("Error: Tox ID Invalida (valor nospam Incorrecto)")
+msgstr("Error: ID de Tox inválida (valor nospam incorrecto)")
 
 msgid(REQ_NO_MEMORY)
-msgstr("Error: Sin Memoria")
+msgstr("Error: No hay memoria suficiente")
+
+msgid(SEND_FILE)
+msgstr("Enviar archivo")
+
+msgid(SAVE_FILE)
+msgstr("Guardar archivo")
+
+msgid(WHERE_TO_SAVE_FILE_PROMPT)
+msgstr("¿Donde quieres guardar \"%.*s\"?")
+
+msgid(SEND_FILE_PROMPT)
+msgstr("Selecciona uno o más archivos para enviar.")
+
+msgid(SCREEN_CAPTURE_PROMPT)
+msgstr("Arrastra el cursor para hacer una caja alrededor del área de la pantalla que quieres enviar.")
 
 msgid(TRANSFER_NEW)
-msgstr("Nueva Transferencia de archivos")
+msgstr("Nueva transferencia de archivos")
 
 msgid(TRANSFER_STARTED)
-msgstr("Transferencia de archivos Iniciada")
+msgstr("Transferencia de archivos iniciada")
+
+msgid(TRANSFER___)
+msgstr("...")
 
 msgid(TRANSFER_PAUSED)
 msgstr("Transferencia de archivos pausada")
 
 msgid(TRANSFER_BROKEN)
-msgstr("Transferencia de archivos interrumpida")
+msgstr("Transferencia de archivos rota")
 
 msgid(TRANSFER_CANCELLED)
 msgstr("Transferencia de archivos cancelada")
@@ -62,16 +80,19 @@ msgid(CALL_CANCELLED)
 msgstr("Llamada cancelada")
 
 msgid(CALL_INVITED)
-msgstr("Llamada entrante")
+msgstr("Llamada invitada")
 
 msgid(CALL_RINGING)
-msgstr("Llamando")
+msgstr("Llamada sonando")
 
 msgid(CALL_STARTED)
 msgstr("Llamada iniciada")
 
 msgid(ADDFRIENDS)
-msgstr("Agregar amigos")
+msgstr("Añadir Amigos")
+
+msgid(TOXID)
+msgstr("ID de Tox")
 
 msgid(MESSAGE)
 msgstr("Mensaje")
@@ -80,79 +101,115 @@ msgid(SEARCHFRIENDS)
 msgstr("Buscar amigos")
 
 msgid(ADD)
-msgstr("Agregar")
+msgstr("Añadir")
+
+msgid(CREATEGROUPCHAT)
+msgstr("Crear Chat Grupal")
 
 msgid(SWITCHPROFILE)
-msgstr("Cambiar perfil")
+msgstr("Cambiar Perfil")
 
 msgid(FRIENDREQUEST)
-msgstr("Solicitudes de amistad")
+msgstr("Solicitud de Amistad")
 
 msgid(USERSETTINGS)
-msgstr("Configuración")
+msgstr("Ajustes del Usuario")
 
 msgid(NAME)
 msgstr("Nombre")
 
+msgid(PROFILE)
+msgstr("Perfil")
+
 msgid(STATUSMESSAGE)
-msgstr("Estado")
+msgstr("Mensaje de Estado")
 
 msgid(PREVIEW)
 msgstr("Vista preliminar")
 
 msgid(DEVICESELECTION)
-msgstr("Seleccionar dispositivo")
+msgstr("Selección de Dispositivo")
 
 msgid(AUDIOINPUTDEVICE)
-msgstr("Seleccionar Entrada de Audio")
+msgstr("Dispositivo de Entrada de Audio")
 
 msgid(AUDIOFILTERING)
 msgstr("Filtrar Audio")
 
 msgid(AUDIOOUTPUTDEVICE)
-msgstr("Seleccionar Salida de Audio")
+msgstr("Dispositivo de Salida de Audio")
 
 msgid(VIDEOINPUTDEVICE)
-msgstr("Seleccionar Entrada de Vídeo")
+msgstr("Dispositivo de Entrada de Vídeo")
 
 msgid(OTHERSETTINGS)
-msgstr("Configuración extra")
+msgstr("Más Ajustes")
+
+msgid(UI)
+msgstr("IU")
+
+msgid(USER_INTERFACE)
+msgstr("Interfaz")
+
+msgid(UTOX_SETTINGS)
+msgstr("Ajustes de uTox")
+
+msgid(NETWORK_SETTINGS)
+msgstr("Ajustes de Red")
+
+msgid(AUDIO_VIDEO)
+msgstr("Audio/Vídeo")
 
 msgid(DPI)
-msgstr("Resolución de PPP")
+msgstr("DPI")
 
 msgid(SAVELOCATION)
-msgstr("Guardar ubicación")
+msgstr("Guardar Ubicación")
 
 msgid(LANGUAGE)
-msgstr("Idioma")
+msgstr("Lenguaje")
 
 msgid(NETWORK)
 msgstr("Red")
 
+msgid(IPV6)
+msgstr("IPv6:")
+
+msgid(UDP)
+msgstr("UDP:")
+
+msgid(PROXY)
+msgstr("Proxy (SOCKS 5)")
+
 msgid(WARNING)
-msgstr("Al Cambiar configuración de la Red/Proxy, sera desconectado temporalmente de la Red Tox")
+msgstr("Cambiar los ajustes de red/proxy te desconectará temporalmente de la red de Tox")
 
 msgid(LOGGING)
-msgstr("Registro")
+msgstr("Registros")
 
 msgid(AUDIONOTIFICATIONS)
-msgstr("Activar notificación audible (tono de llamada)")
+msgstr("Activar Notificaciones por Sonido (Tonos)")
+
+msgid(RINGTONE)
+msgstr("Tono")
 
 msgid(IS_TYPING)
 msgstr("está escribiendo...")
 
 msgid(CLOSE_TO_TRAY)
-msgstr("Minimizar en la bandeja en vez de cierre")
+msgstr("Ocultar en la bandeja al cerrar")
 
 msgid(START_IN_TRAY)
-msgstr("Iniciar minimizado en la bandeja del sistema")
+msgstr("Ocultar en la bandeja por defecto")
 
 msgid(COPY)
 msgstr("Copiar")
 
 msgid(COPYWITHOUTNAMES)
 msgstr("Copiar (sin nombres)")
+
+msgid(COPY_WITH_NAMES)
+msgstr("Copiar (incluir nombres)")
 
 msgid(CUT)
 msgstr("Cortar")
@@ -161,16 +218,28 @@ msgid(PASTE)
 msgstr("Pegar")
 
 msgid(DELETE)
-msgstr("Borrar")
+msgstr("Eliminar")
 
 msgid(SELECTALL)
-msgstr("Seleccionar Todo")
+msgstr("Seleccionar todo")
 
 msgid(REMOVE)
 msgstr("Eliminar")
 
+msgid(REMOVE_FRIEND)
+msgstr("Eliminar Amigo")
+
+msgid(REMOVE_GROUP)
+msgstr("Eliminar Grupo")
+
 msgid(LEAVE)
 msgstr("Salir")
+
+msgid(LEAVE_GROUP)
+msgstr("Salir del grupo")
+
+msgid(CTOPIC)
+msgstr("Cambiar tema")
 
 msgid(ACCEPT)
 msgstr("Aceptar")
@@ -178,80 +247,128 @@ msgstr("Aceptar")
 msgid(IGNORE)
 msgstr("Ignorar")
 
+msgid(SENDSCREENSHOT)
+msgstr("Enviar una captura de pantalla")
+
 msgid(CLICKTOSAVE)
-msgstr("Click para guardar")
+msgstr("Clic para guardar")
 
 msgid(CLICKTOOPEN)
-msgstr("Click para abrir")
+msgstr("Clic para abrir")
 
 msgid(CANCELLED)
 msgstr("Cancelado")
 
 msgid(DPI_TINY)
-msgstr("Diminuto (50%)")
+msgstr("Pequeño (50%)")
 
 msgid(DPI_NORMAL)
-msgstr("Normales (100%)")
+msgstr("Normal (100%)")
 
 msgid(DPI_BIG)
 msgstr("Grande (150%)")
 
 msgid(DPI_LARGE)
-msgstr("Muy grande (200%)")
+msgstr("Más grande (200%)")
 
 msgid(DPI_HUGE)
 msgstr("Enorme (250%)")
 
 msgid(CONTACTS_FILTER_ALL)
-msgstr("Todas")
+msgstr("Todos")
 
 msgid(CONTACTS_FILTER_ONLINE)
-msgstr("En línea")
+msgstr("Conectados")
 
 msgid(PROXY_DISABLED)
-msgstr("Discapacitado")
+msgstr("Desactivado")
 
 msgid(PROXY_FALLBACK)
-msgstr("Si es necesario")
+msgstr("Sólo si no se puede conectar normalmente")
 
 msgid(PROXY_ALWAYS_USE)
-msgstr("Usar siempre")
+msgstr("Siempre usar")
+
+msgid(NO)
+msgstr("No")
 
 msgid(YES)
 msgstr("Sí")
 
+msgid(OFF)
+msgstr("Desactivado")
+
+msgid(ON)
+msgstr("Activado")
+
 msgid(VIDEO_IN_NONE)
-msgstr("Nada")
+msgstr("Ninguno")
 
 msgid(VIDEO_IN_DESKTOP)
 msgstr("Escritorio")
 
 msgid(AUDIO_IN_DEFAULT_LOOPBACK)
-msgstr("Bucle invertido ordinario")
+msgstr("Loopback")
+
+msgid(AUDIO_IN_ANDROID)
+msgstr("Entrade de OpenSL")
 
 msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
-msgstr("Acepte esta solicitud de amistad por favor.")
+msgstr("Por favor, acepta esta solicitud de amistad.")
 
 msgid(CONTACTS_FILTER_EDIT_HINT)
 msgstr("Buscar amigos")
+
+msgid(PROXY_EDIT_HINT_IP)
+msgstr("IP")
 
 msgid(PROXY_EDIT_HINT_PORT)
 msgstr("Puerto")
 
 msgid(WINDOW_TITLE_VIDEO_PREVIEW)
-msgstr("Vista previa de vídeo")
+msgstr("Vista preliminar del vídeo")
 
 msgid(MUTE)
-msgstr("Enmudecer")
+msgstr("Silenciar")
 
 msgid(UNMUTE)
-msgstr("Desenmudecer")
+msgstr("Desilenciar")
+
+msgid(SELECT_AVATAR_TITLE)
+msgstr("Seleccionar imagen de perfil")
 
 msgid(AVATAR_TOO_LARGE_MAX_SIZE_IS)
-msgstr("Imagen es demasiado grande. Tamaño máximo: ")
+msgstr("Imagen de perfil demasiado grande. Tamaño máximo: ")
 
 msgid(CANT_FIND_FILE_OR_EMPTY)
-msgstr("No se puede encontrar el archivo seleccionado o archivo seleccionado está vacío.")
+msgstr("No se pudo encontrar el archivo seleccionado, o el archivo está vacío.")
 
 msgid(CLEAR_HISTORY)
-msgstr("Borrar el historial")
+msgstr("Limpiar historial")
+
+msgid(AUTO_STARTUP)
+msgstr("Iniciar automáticamente cuando se encienda el ordenador")
+
+msgid(THEME)
+msgstr("Tema")
+
+msgid(THEME_DEFAULT)
+msgstr("Por defecto")
+
+msgid(THEME_LIGHT)
+msgstr("Tema claro")
+
+msgid(THEME_DARK)
+msgstr("Tema oscuro")
+
+msgid(THEME_HIGHCONTRAST)
+msgstr("Alto contraste")
+
+msgid(THEME_CUSTOM)
+msgstr("Personalizado (lee la documentación)")
+
+msgid(THEME_ZENBURN)
+msgstr("Zenburn")
+
+msgid(SEND_TYPING_NOTIFICATIONS)
+msgstr("Mostrar cuándo estoy escribiendo")
