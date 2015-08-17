a69dc5878cf6f53914becde3d1037707777d9c82
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2015-08-17 01:48:40 -0700

    don't double return a call exit

--- utox_av.c
+++ utox_av.c
@@ -67,7 +67,10 @@ void utox_av_local_disconnect(ToxAV *av, int32_t friend_number) {
     TOXAV_ERR_CALL_CONTROL error = 0;
     toxav_call_control(av, friend_number, TOXAV_CALL_CONTROL_CANCEL, &error);
     if (error) {
-        debug("unhanded error in utox_av_end (%i)\n", error);
+        if (error == 2) {
+            return;
+        }
+        debug("ToxAV:\tunhanded error in utox_av_end (%i)\n", error);
     }
     FRIEND *f = &friend[friend_number];
     f->call_state_self = 0;
