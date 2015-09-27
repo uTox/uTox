01b06902ed2d70fcf6760019bf25423c36252b87
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2015-09-27 14:32:01 -0700

    code cleanups

--- main.h
+++ main.h
@@ -55,9 +55,9 @@
 
 #define isdesktop(x) ((size_t)(x) == 1)
 
-// Structs
-typedef struct
-{
+/* House keeping for uTox save file. */
+#define SAVE_VERSION 3
+typedef struct {
     uint8_t version, scale, enableipv6, disableudp;
     uint16_t window_x, window_y, window_width, window_height;
     uint16_t proxy_port;
@@ -77,15 +77,14 @@ typedef struct
     uint8_t zero : 7;
     uint16_t unused[31];
     uint8_t proxy_ip[0];
-}UTOX_SAVE;
+} UTOX_SAVE;
 
-#define SAVE_VERSION 3
+// Structs
 
-typedef struct
-{
+typedef struct {
     uint16_t length;
     uint8_t id[TOX_FRIEND_ADDRESS_SIZE], msg[0];
-}FRIENDREQ;
+} FRIENDREQ;
 
 typedef struct {
     // Castless wrapper for lodepng data arguments.
@@ -95,8 +94,7 @@ typedef struct {
 typedef struct edit_change EDIT_CHANGE;
 
 // Enums
-enum
-{
+enum {
     CURSOR_NONE,
     CURSOR_TEXT,
     CURSOR_HAND,
@@ -120,7 +118,7 @@ enum {
     FONT_MISC,
 };
 
-//sysmenu icons
+/* SVG Bitmap names. */
 enum {
     BM_ONLINE = 1,
     BM_AWAY,
@@ -141,6 +139,7 @@ enum {
     BM_GROUP,
 
     BM_FILE,
+    BM_FILE_BIG,
     BM_CALL,
     BM_VIDEO,
 
