fcf826a96b24ca96de9d5cf2a06c02819fa1f253
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2015-10-13 08:57:36 -0700

    don't delete friends by mistake

--- windows/main.c
+++ windows/main.c
@@ -1627,13 +1627,7 @@ LRESULT CALLBACK WindowProc(HWND hwn, UINT msg, WPARAM wParam, LPARAM lParam)
             }
         } else {
             messages_char(wParam);
-
-            switch(wParam) {
-            case VK_DELETE: {
-                list_deletesitem();
-                return 0;
-            }
-            }
+            return 0;
         }
 
         return 0;
--- xlib/event.c
+++ xlib/event.c
@@ -362,10 +362,6 @@ _Bool doevent(XEvent event)
             }
         }
 
-        if(sym == XK_Delete) {
-            list_deletesitem();
-        }
-
         break;
     }
 
