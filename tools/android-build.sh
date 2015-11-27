5b8b4d3f1f87782781689d291c7e1ddd06946691
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2015-11-26 21:21:57 -0800

    change stuff

--- tools/android-build.sh
+++ tools/android-build.sh
@@ -55,7 +55,7 @@ arm-linux-androideabi-gcc -Wl,--error-unresolved-symbols \
 		./*.c ./png/png.c -llog -landroid -lEGL -lGLESv2 $OPENAL_BUILD \
 		../toxcore-arm/lib/libtoxcore.a ../toxcore-arm/lib/libtoxdns.a ../toxcore-arm/lib/libtoxav.a ../toxcore-arm/lib/libsodium.a \
 		../toxcore-arm/lib/libopus.a ../toxcore-arm/lib/libvpx.a ../freetype-arm/lib/libfreetype.a \
-		-lm -lz -ldl -shared -o ./tmp/libs/armeabi/libn.so
+		-lm -lz -ldl -shared -o ./tmp/libs/armeabi/libuTox.so
 
 $SDK_PATH/build-tools/21.1.2/aapt package -f -M ./android/AndroidManifest.xml -S ./android/res \
 		-I $SDK_PATH/platforms/android-21/android.jar -F ./tmp/tmp1.apk -J ./tmp/java
