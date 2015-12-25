e3f2a0d407a9fa062eac680fa906f2f7d78e31ad
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2015-12-25 13:03:17 -0800

    fix alt + f4 on windows

--- src/windows/main.c
+++ src/windows/main.c
@@ -1663,10 +1663,10 @@ LRESULT CALLBACK WindowProc(HWND hwn, UINT msg, WPARAM wParam, LPARAM lParam)
             }
         } else {
             messages_char(wParam);
-            return 0;
+            break;
         }
 
-        return 0;
+        break;
     }
 
     case WM_CHAR: {
