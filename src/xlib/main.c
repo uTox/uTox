df92cf22371520050ae3c49cc9bad4afc0b19fb7
Dmitrij D. Czarkoff <czarkoff@gmail.com>
2016-01-04 15:32:46 +0100

    Fix OpenBSD compatibility
    In Makefile:
    
     * Apply all CFLAGS and LDFLAGS configuration options unconditionally.
       Platform-specific overrides may be applied on top of that.
     * Don't add "-lresolv -ldl" to OpenBSD, where they are not needed.
     * Allow picking pkg-config implementation via PKG_CONFIG macro.
    
    In src/xlib/main.c:
     * Enclose Linux-specific mechanism for handling key input inside ifdef.

--- src/xlib/main.c
+++ src/xlib/main.c
@@ -1,5 +1,9 @@
 #include "../main.h"
 
+#ifdef LINUX_IO
+#include <linux/input.h>
+#endif
+
 _Bool    hidden = 0;
 uint32_t tray_width = 32, tray_height = 32;
 XIC xic = NULL;
@@ -74,7 +78,6 @@ void postmessage(uint32_t msg, uint16_t param1, uint16_t param2, void *data)
 }
 
 
-#include <linux/input.h>
 FILE *ptt_keyboard_handle;
 Display *ptt_display;
 void init_ptt(void){
@@ -99,6 +102,7 @@ _Bool check_ptt_key(void){
     }
     int ptt_key;
 
+#ifdef LINUX_IO
     /* First, we try for direct access to the keyboard. */
     ptt_key = KEY_LEFTCTRL;                                      // TODO allow user to change this...
     if (ptt_keyboard_handle) {
@@ -117,6 +121,8 @@ _Bool check_ptt_key(void){
             return 0;
         }
     }
+#endif /* LINUX_IO */
+
     /* Okay nope, lets' fallback to xinput... *pouts*
      * Fall back to Querying the X for the current keymap. */
     ptt_key = XKeysymToKeycode(display, XK_Control_L);
