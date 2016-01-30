e137eb8f7504fb304e678b71fc2e197bfd9104a0
Heaven has fox ears <stal@zodiaclabs.org>
2016-01-29 23:17:29 -0800

    fix segfaults

--- src/cocoa/main.m
+++ src/cocoa/main.m
@@ -118,7 +118,7 @@ void thread(void func(void*), void *args) {
     pthread_t thread_temp;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_attr_setstacksize(&attr, 1 << 18);
+    pthread_attr_setstacksize(&attr, 1 << 20);
     pthread_create(&thread_temp, &attr, (void*(*)(void*))func, args);
     pthread_attr_destroy(&attr);
 }
