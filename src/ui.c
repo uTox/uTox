40993d5defa3a23fe45241b250e4a1b40e36992c
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-03-02 17:34:48 -0800

    add toxcore toxav toxes to settings header

--- src/ui.c
+++ src/ui.c
@@ -270,14 +270,21 @@ static void draw_profile_password(int UNUSED(x), int UNUSED(y), int UNUSED(w), i
 }
 
 /* Top bar for user settings */
-static void draw_settings_header(int UNUSED(x), int UNUSED(y), int UNUSED(width), int UNUSED(height)){
+static void draw_settings_header(int UNUSED(x), int UNUSED(y), int w, int UNUSED(height)){
     setcolor(COLOR_MAIN_TEXT);
     setfont(FONT_SELF_NAME);
-    drawstr(MAIN_LEFT + UTOX_SCALE(5), UTOX_SCALE(5), UTOX_SETTINGS);
+    drawstr(MAIN_LEFT + SCALE(10), SCALE(10), UTOX_SETTINGS);
     #ifdef GIT_VERSION
-        int x = MAIN_LEFT + UTOX_SCALE(5 ) + UTOX_STR_WIDTH(UTOX_SETTINGS) + UTOX_SCALE(5 );
+        int x = MAIN_LEFT + SCALE(10) + UTOX_STR_WIDTH(UTOX_SETTINGS) + SCALE(10);
         setfont(FONT_TEXT);
-        drawtext(x, UTOX_SCALE(5), (uint8_t*)GIT_VERSION, strlen(GIT_VERSION));
+        drawtext(x, SCALE(10), (uint8_t*)GIT_VERSION, strlen(GIT_VERSION));
+        char version_string[64];
+        int count;
+        count = snprintf(version_string, 64, "Core v%u.%u.%u ToxAV v%u.%u.%u ToxES v%u.%u.%u",
+                                    tox_version_major(),   tox_version_minor(),   tox_version_patch(),
+                                    toxav_version_major(), toxav_version_minor(), toxav_version_patch(),
+                                    toxes_version_major(), toxes_version_minor(), toxes_version_patch());
+        drawtextwidth_right(w, textwidth((char_t*)version_string, count), SCALE(10), (uint8_t*)version_string, strlen(version_string));
     #endif
 }
 
