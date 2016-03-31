54e386861818747bb736a45bf90b946a6a1b2c3e
lineteen <lineteen@msn.com>
2016-04-01 00:30:02 +0800

    Update cn.h
    Old Chinese language file is not complete.

--- langs/cn.h
+++ langs/cn.h
@@ -5,81 +5,118 @@ msgid(LANG_ENGLISH_NAME)
 msgstr("SIMPLIFIED CHINESE")
 
 msgid(REQ_SENT)
-msgstr("添加好友请求已发送。当对方接受请求后将在好友列表中在线。")
+msgstr("好友请求已发送。对方接受之后将显示为在线状态。")
 
 msgid(REQ_RESOLVE)
-msgstr("尝试解析DNS名......")
+msgstr("正在尝试解析该DNS域名……")
+
+msgid(DNS_DISABLED)
+msgstr("因使用非UDP服务的代理，DNS域名解析被禁用！")
 
 msgid(REQ_INVALID_ID)
-msgstr("错误： 无效的 Tox ID")
+msgstr("错误：无效的 Tox ID")
 
 msgid(REQ_EMPTY_ID)
-msgstr("错误： 没有指定Tox ID")
+msgstr("错误：未指定 Tox ID")
 
 msgid(REQ_LONG_MSG)
-msgstr("错误： 消息太长")
+msgstr("错误：消息过长")
 
 msgid(REQ_NO_MSG)
-msgstr("错误： 空消息")
+msgstr("错误：空消息")
 
 msgid(REQ_SELF_ID)
-msgstr("错误： Tox ID 是你自己的 ID")
+msgstr("错误：TOX ID 是你自己的账号")
 
 msgid(REQ_ALREADY_FRIENDS)
-msgstr("错误： Tox ID 已在好友列表中了")
+msgstr("错误：该 Tox ID 已存在于好友列表中")
 
 msgid(REQ_UNKNOWN)
-msgstr("错误： 未知")
+msgstr("错误：未知")
 
 msgid(REQ_BAD_CHECKSUM)
-msgstr("错误： 无效的 Tox ID (错误的校验和)")
+msgstr("错误：无效的 Tox ID（错误的校检码）")
 
 msgid(REQ_BAD_NOSPAM)
-msgstr("错误： 无效的 Tox ID (错误的nospam值)")
+msgstr("错误：无效的 Tox ID（错误的防骚扰码）")
 
 msgid(REQ_NO_MEMORY)
-msgstr("错误： 没内存了")
+msgstr("错误：内存不足")
+
+msgid(SEND_FILE)
+msgstr("传送文件")
+
+msgid(SAVE_FILE)
+msgstr("保存文件")
+
+msgid(WHERE_TO_SAVE_FILE_PROMPT)
+msgstr("你要将 \"%.*s\"保存在哪里？")
+
+msgid(WHERE_TO_SAVE_FILE)
+msgstr("你要将文件保存在哪里？")
+
+msgid(SEND_FILE_PROMPT)
+msgstr("选择一个或多个文件以传送。")
+
+msgid(SCREEN_CAPTURE_PROMPT)
+msgstr("在屏幕上的目标区域截屏。")
 
 msgid(TRANSFER_NEW)
-msgstr("新建文件传输任务")
+msgstr("新建文件传送任务")
 
 msgid(TRANSFER_STARTED)
-msgstr("文件传输开始")
+msgstr("文件传送开始")
 
-//TODO: Same as in English? Localize or remove.
-//msgid(TRANSFER___)
-//msgstr("...")
+msgid(TRANSFER___)
+msgstr("……")
 
 msgid(TRANSFER_PAUSED)
-msgstr("文件传输暂停")
+msgstr("文件传送暂停")
 
 msgid(TRANSFER_BROKEN)
-msgstr("文件传输中断")
+msgstr("文件传送中断")
 
 msgid(TRANSFER_CANCELLED)
-msgstr("文件传输取消")
+msgstr("文件传送取消")
 
 msgid(TRANSFER_COMPLETE)
-msgstr("文件传输完成")
+msgstr("文件传送完成")
+
+msgid(GROUPCHAT_JOIN_AUDIO)
+msgstr("参加语音聊天")
+
+msgid(START_AUDIO_CALL)
+msgstr("语音呼叫")
+
+msgid(START_VIDEO_CALL)
+msgstr("视频呼叫")
 
 msgid(CALL_CANCELLED)
 msgstr("呼叫取消")
 
 msgid(CALL_INVITED)
-msgstr("呼叫邀请")
+msgstr("呼叫请求")
 
 msgid(CALL_RINGING)
-msgstr("呼叫振铃")
+msgstr("呼叫响铃")
 
 msgid(CALL_STARTED)
 msgstr("呼叫开始")
 
+msgid(PROFILE_SETTINGS)
+msgstr("用户设置")
+
+msgid(PROFILE_PW_WARNING)
+msgstr("注意：µTox 启动后将必须提供该密码。")
+
+msgid(PROFILE_PW_NO_RECOVER)
+msgstr("忘记密码将无法找回。")
+
 msgid(ADDFRIENDS)
 msgstr("添加好友")
 
-//TODO: Same as in English? Localize or remove.
-//msgid(TOXID)
-//msgstr("Tox ID")
+msgid(TOXID)
+msgstr("Tox ID")
 
 msgid(MESSAGE)
 msgstr("消息")
@@ -87,23 +124,47 @@ msgstr("消息")
 msgid(SEARCHFRIENDS)
 msgstr("搜索好友")
 
+msgid(FILTER_ONLINE)
+msgstr("在线好友")
+
+msgid(FILTER_ALL)
+msgstr("全部好友")
+
+msgid(FILTER_CONTACT_TOGGLE)
+msgstr("切换是否过滤离线好友")
+
+msgid(FILTER_TO_ALL)
+msgstr("显示全部好友！")
+
+msgid(FILTER_TO_ONLINE)
+msgstr("仅显示在线好友！")
+
 msgid(ADD)
 msgstr("添加")
 
+msgid(CREATEGROUPCHAT)
+msgstr("创建一个群聊")
+
 msgid(SWITCHPROFILE)
-msgstr("切换设置")
+msgstr("切换配置文件")
 
 msgid(FRIENDREQUEST)
-msgstr("好友呼叫")
+msgstr("好友请求")
 
 msgid(USERSETTINGS)
 msgstr("用户设置")
 
+msgid(FRIEND_SETTINGS)
+msgstr("好友设置")
+
 msgid(NAME)
 msgstr("名字")
 
+msgid(PROFILE)
+msgstr("配置")
+
 msgid(STATUSMESSAGE)
-msgstr("状态消息（签名）")
+msgstr("签名")
 
 msgid(PREVIEW)
 msgstr("预览")
@@ -114,18 +175,65 @@ msgstr("设备选择")
 msgid(AUDIOINPUTDEVICE)
 msgstr("音频输入设备")
 
+msgid(AUDIOFILTERING)
+msgstr("音频过滤")
+
 msgid(AUDIOOUTPUTDEVICE)
 msgstr("音频输出设备")
 
 msgid(VIDEOINPUTDEVICE)
 msgstr("视频输入设备")
 
+msgid(PUSH_TO_TALK)
+msgstr("一键对讲模式")
+
+msgid(STATUS)
+msgstr("状态")
+
+msgid(STATUS_ONLINE)
+msgstr("在线")
+
+msgid(STATUS_AWAY)
+msgstr("离开")
+
+msgid(STATUS_BUSY)
+msgstr("忙碌")
+
+msgid(NOT_CONNECTED)
+msgstr("未连线")
+
 msgid(OTHERSETTINGS)
 msgstr("其他设置")
 
-//TODO: Same as in English? Localize or remove.
-//msgid(DPI)
-//msgstr("DPI")
+msgid(UI)
+msgstr("用户界面")
+
+msgid(USER_INTERFACE)
+msgstr("界面")
+
+msgid(UTOX_SETTINGS)
+msgstr("uTox设置")
+
+msgid(NETWORK_SETTINGS)
+msgstr("网络设置")
+
+msgid(PROFILE_PASSWORD)
+msgstr("用户密码")
+
+msgid(LOCK_UTOX)
+msgstr("断开 Tox 并锁定用户。")
+
+msgid(SHOW_UI_PASSWORD)
+msgstr("点击显示密码配置栏。此处所作的更改将即刻生效！")
+
+msgid(LOCK)
+msgstr("锁定")
+
+msgid(AUDIO_VIDEO)
+msgstr("音/视频")
+
+msgid(DPI)
+msgstr("分辨率缩放")
 
 msgid(SAVELOCATION)
 msgstr("保存位置")
@@ -136,31 +244,44 @@ msgstr("语言")
 msgid(NETWORK)
 msgstr("网络")
 
-//TODO: Same as in English? Localize or remove.
-//msgid(IPV6)
-//msgstr("IPv6:")
+msgid(IPV6)
+msgstr("IPv6:")
 
-//TODO: Same as in English? Localize or remove.
-//msgid(UDP)
-//msgstr("UDP:")
+msgid(UDP)
+msgstr("UDP:")
 
 msgid(PROXY)
 msgstr("代理 (SOCKS 5)")
 
 msgid(WARNING)
-msgstr("修改网络或代理设置将会断开Tox网络连接")
+msgstr("修改网络或代理设置将会暂时断开Tox的网络连接")
 
 msgid(LOGGING)
-msgstr("记录")
+msgstr("保留记录")
+
+msgid(AUDIONOTIFICATIONS)
+msgstr("启用声音通知 (响铃)")
+
+msgid(RINGTONE)
+msgstr("声音")
 
 msgid(IS_TYPING)
-msgstr("正在输入...")
+msgstr("正在打字...")
+
+msgid(CLOSE_TO_TRAY)
+msgstr("关闭后运行于托盘区")
+
+msgid(START_IN_TRAY)
+msgstr("最小化启动于托盘区")
 
 msgid(COPY)
 msgstr("复制")
 
 msgid(COPYWITHOUTNAMES)
-msgstr("复制 (不带名字)")
+msgstr("复制 (不包含名字)")
+
+msgid(COPY_WITH_NAMES)
+msgstr("复制 (包含名字)")
 
 msgid(CUT)
 msgstr("剪切")
@@ -175,16 +296,43 @@ msgid(SELECTALL)
 msgstr("全选")
 
 msgid(REMOVE)
-msgstr("删除")
+msgstr("移除")
+
+msgid(REMOVE_FRIEND)
+msgstr("移除好友")
+
+msgid(REMOVE_GROUP)
+msgstr("移除群组")
 
 msgid(LEAVE)
 msgstr("离开")
+
+msgid(LEAVE_GROUP)
+msgstr("离开群组")
+
+msgid(CTOPIC)
+msgstr("更改主题")
 
 msgid(ACCEPT)
 msgstr("接受")
 
 msgid(IGNORE)
 msgstr("忽略")
+
+msgid(SET_ALIAS)
+msgstr("设置别名")
+
+msgid(ALIAS)
+msgstr("别名")
+
+msgid(FRIEND_AUTOACCEPT)
+msgstr("无须确认直接接收其传送的文件")
+
+msgid(SENDMESSAGE)
+msgstr("发送消息")
+
+msgid(SENDSCREENSHOT)
+msgstr("发送截屏")
 
 msgid(CLICKTOSAVE)
 msgstr("点击保存")
@@ -194,6 +342,81 @@ msgstr("点击打开")
 
 msgid(CANCELLED)
 msgstr("取消")
+
+msgid(DPI_060)
+msgstr("缩放至 60%")
+
+msgid(DPI_070)
+msgstr("缩放至 70%")
+
+msgid(DPI_080)
+msgstr("缩放至 80%")
+
+msgid(DPI_090)
+msgstr("缩放至 90%")
+
+msgid(DPI_100)
+msgstr("缩放至 100%")
+
+msgid(DPI_110)
+msgstr("缩放至 110%")
+
+msgid(DPI_120)
+msgstr("缩放至 120%")
+
+msgid(DPI_130)
+msgstr("缩放至 130%")
+
+msgid(DPI_140)
+msgstr("缩放至 140%")
+
+msgid(DPI_150)
+msgstr("缩放至 150%")
+
+msgid(DPI_160)
+msgstr("缩放至 160%")
+
+msgid(DPI_170)
+msgstr("缩放至 170%")
+
+msgid(DPI_180)
+msgstr("缩放至 180%")
+
+msgid(DPI_190)
+msgstr("缩放至 190%")
+
+msgid(DPI_200)
+msgstr("缩放至 200%")
+
+msgid(DPI_210)
+msgstr("缩放至 210%")
+
+msgid(DPI_220)
+msgstr("缩放至 220%")
+
+msgid(DPI_230)
+msgstr("缩放至 230%")
+
+msgid(DPI_240)
+msgstr("缩放至 240%")
+
+msgid(DPI_250)
+msgstr("缩放至 250%")
+
+msgid(DPI_260)
+msgstr("缩放至 260%")
+
+msgid(DPI_270)
+msgstr("缩放至 270%")
+
+msgid(DPI_280)
+msgstr("缩放至 280%")
+
+msgid(DPI_290)
+msgstr("缩放至 290%")
+
+msgid(DPI_300)
+msgstr("缩放至 300%")
 
 msgid(DPI_TINY)
 msgstr("小 (50%)")
@@ -210,22 +433,14 @@ msgstr("较大 (200%)")
 msgid(DPI_HUGE)
 msgstr("极大 (250%)")
 
-msgid(FILTER_ALL)
-msgstr("全部")
-
-msgid(FILTER_ONLINE)
-msgstr("在线")
-
 msgid(PROXY_DISABLED)
-msgstr("已禁用")
+msgstr("已弃用")
 
-//TODO: Localize or remove.
-//msgid(PROXY_FALLBACK)
-//msgstr("Fallback")
+msgid(PROXY_FALLBACK)
+msgstr("逐渐退去")
 
-//TODO: Localize or remove.
-//msgid(PROXY_ALWAYS_USE)
-//msgstr("Always use")
+msgid(PROXY_ALWAYS_USE)
+msgstr("经常使用")
 
 msgid(NO)
 msgstr("否")
@@ -233,29 +448,86 @@ msgstr("否")
 msgid(YES)
 msgstr("是")
 
+msgid(OFF)
+msgstr("关")
+
+msgid(ON)
+msgstr("开")
+
+msgid(SHOW)
+msgstr("显示")
+
+msgid(HIDE)
+msgstr("隐藏")
+
 msgid(VIDEO_IN_NONE)
 msgstr("无")
 
-//TODO: Localize or remove.
-//msgid(VIDEO_IN_DESKTOP)
-//msgstr("Desktop")
+msgid(VIDEO_IN_DESKTOP)
+msgstr("桌面")
+
+msgid(AUDIO_IN_DEFAULT_LOOPBACK)
+msgstr("默认环绕")
+
+msgid(AUDIO_IN_ANDROID)
+msgstr("OpenSL输入")
 
 msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
-msgstr("请接受这条好友请求.")
+msgstr("请接受这条好友请求。")
 
-//TODO: Localize or remove.
-//msgid(PROXY_EDIT_HINT_IP)
-//msgstr("IP")
+msgid(CONTACT_SEARCH_ADD_HINT)
+msgstr("搜索/添加好友")
+
+msgid(PROXY_EDIT_HINT_IP)
+msgstr("IP")
 
 msgid(PROXY_EDIT_HINT_PORT)
 msgstr("端口")
 
-//TODO: Localize or remove.
-//msgid(AUDIO_IN_DEFAULT_LOOPBACK)
-//msgstr("Default Loopback")
-
 msgid(WINDOW_TITLE_VIDEO_PREVIEW)
 msgstr("视频预览")
 
-msgid(AUDIONOTIFICATIONS)
-msgstr("启用声音通知 (铃声)")
+msgid(MUTE)
+msgstr("开启静音")
+
+msgid(UNMUTE)
+msgstr("取消静音")
+
+msgid(SELECT_AVATAR_TITLE)
+msgstr("选择头像")
+
+msgid(AVATAR_TOO_LARGE_MAX_SIZE_IS)
+msgstr("头像过大。最大允许尺寸为： ")
+
+msgid(CANT_FIND_FILE_OR_EMPTY)
+msgstr("无法找到所选文件或所选文件为空。")
+
+msgid(CLEAR_HISTORY)
+msgstr("清空历史")
+
+msgid(AUTO_STARTUP)
+msgstr("开机时自动启动")
+
+msgid(THEME)
+msgstr("界面风格")
+
+msgid(THEME_DEFAULT)
+msgstr("默认")
+
+msgid(THEME_LIGHT)
+msgstr("轻淡风格")
+
+msgid(THEME_DARK)
+msgstr("深暗风格")
+
+msgid(THEME_HIGHCONTRAST)
+msgstr("高对比度")
+
+msgid(THEME_CUSTOM)
+msgstr("自定义 (参见文档)")
+
+msgid(THEME_ZENBURN)
+msgstr("低调风")
+
+msgid(SEND_TYPING_NOTIFICATIONS)
+msgstr("发送正在打字的提醒")
