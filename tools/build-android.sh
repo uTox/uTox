d75c84a7df5e51637e79b43257a859bbc983a37c
Felix Salfelder <felix@salfelder.org>
2016-04-07 12:32:45 +0100

    fix TOOLCHAIN default setting

--- tools/build-android.sh
+++ tools/build-android.sh
@@ -23,7 +23,7 @@ ANDROID_NDK_HOME=${ANDROID_NDK_HOME-/opt/android-ndk}
 ANDROID_SDK_HOME=${ANDROID_SDK_HOME-/opt/android-sdk}
 SYSROOT=${SYSROOT-/opt/android-ndk/platforms/${NDK_VERSION}/arch-arm}
 AND_LINKS=${AND_LINKS-" ./lib/arm/lib/lib*.a"}
-TOOLCHAIN=${TOOLCHAIN-./toolchain/bin}
+TOOLCHAIN=${TOOLCHAIN-./toolchain}
 AAPT=${AAPT-/opt/android-sdk/build-tools/${DEV_VERSION}/aapt}
 
 [ -d ${TOOLCHAIN} ] || "$ANDROID_NDK_HOME/build/tools/make-standalone-toolchain.sh"     \
