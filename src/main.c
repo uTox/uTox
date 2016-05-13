c8466343dd2b464b05bc9851eb6269e3c21cd009
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-05-13 12:02:25 -0700

    fix loading own avatar

--- src/main.c
+++ src/main.c
@@ -195,11 +195,11 @@ _Bool utox_save_data_avatar(uint32_t friend_number, const uint8_t *data, size_t 
     uint8_t name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")];
 
     if (friend_number == -1) {
+        memcpy(hex, self.id_buffer, TOX_PUBLIC_KEY_SIZE * 2);
+    } else {
         /* load current user's avatar */
         FRIEND *f = &friend[friend_number];
         cid_to_string(hex, f->cid);
-    } else {
-        memcpy(hex, self.id_buffer, TOX_PUBLIC_KEY_SIZE * 2);
     }
     snprintf((char*)name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"),
              "avatars/%.*s.png", TOX_PUBLIC_KEY_SIZE * 2, (char*)hex);
