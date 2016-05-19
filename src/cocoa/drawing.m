0cb4a4974c21a5bb49cb496706ef4afd4dc637fa
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-05-19 04:11:00 +0000

    blind fix for OSX
    @stal888 I have no idea what any of this does... good luck mate

--- src/cocoa/drawing.m
+++ src/cocoa/drawing.m
@@ -569,3 +569,27 @@ void draw_image(const UTOX_NATIVE_IMAGE *image, int x, int y, uint32_t width, ui
     CGContextDrawImage(this, rect, di);
     CFRelease(di);
 }
+
+void draw_image_inline(uint8_t *img_data, size_t size, uint16_t w, uint16_t h, int x, int y) {
+    DRAW_TARGET_CHK()
+
+    //debug("%lu %lu %lf", imgx, imgy, image->scale);
+
+    CGFloat sz = currently_drawing_into_view.frame.size.height;
+
+    CGRect rect = {
+        .origin = {
+            .x = x,
+            .y = sz - y - height,
+        },
+        .size = {
+            .width  = w,
+            .height = h,
+        }
+    };
+
+    CGContextRef this = [NSGraphicsContext currentContext].graphicsPort;
+    CGImageRef di = CGImageCreateWithImageInRect(image->image, (CGRect){0, 0, w, h});
+    CGContextDrawImage(this, rect, di);
+    CFRelease(di);
+}
