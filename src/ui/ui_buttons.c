b2f2f4b6362c2645c4bea2302d43ddc605348a4e
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-06-02 18:53:22 -0700

    seperate settings into new file

--- src/ui/ui_buttons.c
+++ src/ui/ui_buttons.c
@@ -44,15 +44,23 @@ static void button_avatar_onpress(void) {
         openfileavatar();
 }
 
-static void button_settings_sub_profile_onpress(void);
-
 static void button_name_onpress(void){
-    button_settings_sub_profile_onpress();
+    list_selectsettings();
+    panel_settings_profile.disabled = 0;
+    panel_settings_devices.disabled = 1;
+    panel_settings_net.disabled     = 1;
+    panel_settings_ui.disabled      = 1;
+    panel_settings_av.disabled      = 1;
     edit_setfocus(&edit_name);
 }
 
 static void button_statusmsg_onpress(void){
-    button_settings_sub_profile_onpress();
+    list_selectsettings();
+    panel_settings_profile.disabled = 0;
+    panel_settings_devices.disabled = 1;
+    panel_settings_net.disabled     = 1;
+    panel_settings_ui.disabled      = 1;
+    panel_settings_av.disabled      = 1;
     edit_setfocus(&edit_status);
 }
 
@@ -91,12 +99,6 @@ static void button_add_new_contact_onpress(void) {
         edit_setstr(&edit_search, (char_t *)"", 0);
         list_selectaddfriend();
         edit_setfocus(&edit_add_msg);
-    }
-}
-
-static void button_settings_onpress(void) {
-    if (tox_thread_init) {
-        list_selectsettings();
     }
 }
 
@@ -162,48 +164,6 @@ static void button_videopreview_update(BUTTON *b) {
 static void button_send_friend_request_onpress(void) {
     friend_add(edit_add_id.data, edit_add_id.length, edit_add_msg.data, edit_add_msg.length);
     edit_resetfocus();
-}
-
-extern SCROLLABLE scrollbar_settings;
-
-static void disable_all_setting_sub(void) {
-    list_selectsettings();
-    panel_settings_profile.disabled = 1;
-    panel_settings_devices.disabled = 1;
-    panel_settings_net.disabled     = 1;
-    panel_settings_ui.disabled      = 1;
-    panel_settings_av.disabled      = 1;
-
-}
-
-static void button_settings_sub_profile_onpress(void){
-    scrollbar_settings.content_height = SCALE(260);
-    disable_all_setting_sub();
-    panel_settings_profile.disabled = 0;
-}
-
-static void button_settings_sub_devices_onpress(void){
-    scrollbar_settings.content_height = SCALE(260);
-    disable_all_setting_sub();
-    panel_settings_devices.disabled = 0;
-}
-
-static void button_settings_sub_net_onpress(void){
-    scrollbar_settings.content_height = SCALE(180);
-    disable_all_setting_sub();
-    panel_settings_net.disabled = 0;
-}
-
-static void button_settings_sub_ui_onpress(void){
-    scrollbar_settings.content_height = SCALE(280);
-    disable_all_setting_sub();
-    panel_settings_ui.disabled = 0;
-}
-
-static void button_settings_sub_av_onpress(void){
-    scrollbar_settings.content_height = SCALE(300);
-    disable_all_setting_sub();
-    panel_settings_av.disabled = 0;
 }
 
 static void button_group_audio_onpress(void) {
@@ -311,19 +271,6 @@ static void button_call_video_update(BUTTON *b) {
             b->disabled = 1;
         }
     }
-}
-
-static void button_bottommenu_update(BUTTON *b) {
-    b->c1 = COLOR_BACKGROUND_MENU;
-    b->c2 = COLOR_BACKGROUND_MENU_HOVER;
-    b->c3 = COLOR_BACKGROUND_MENU_ACTIVE;
-    b->ct1 = COLOR_MENU_TEXT;
-    b->ct2 = COLOR_MENU_TEXT;
-    if (b->mousedown || b->disabled) {
-        b->ct1 = COLOR_MENU_ACTIVE_TEXT;
-        b->ct2 = COLOR_MENU_ACTIVE_TEXT;
-    }
-    b->cd = COLOR_BACKGROUND_MENU_ACTIVE;
 }
 
 static void button_accept_friend_onpress(void) {
@@ -465,48 +412,6 @@ button_add_new_contact = {
     .nodraw       = 1,
     .tooltip_text = { .i18nal = STR_ADDFRIENDS },
 },
-
-button_settings = {
-    .bm2          = BM_SETTINGS,
-    .bw           = _BM_ADD_WIDTH,
-    .bh           = _BM_ADD_WIDTH,
-    .update       = button_bottommenu_update,
-    .onpress      = button_settings_onpress,
-    .disabled     = 0,
-    .nodraw       = 0,
-    .tooltip_text = { .i18nal = STR_USERSETTINGS },
-},
-
-button_settings_sub_profile = {
-    .nodraw       = 1,
-    .onpress      = button_settings_sub_profile_onpress,
-    .tooltip_text = { .i18nal = STR_UTOX_SETTINGS },
-},
-
-button_settings_sub_devices = {
-    .nodraw       = 1,
-    .onpress      = button_settings_sub_devices_onpress,
-    .tooltip_text = { .i18nal = STR_UTOX_SETTINGS },
-},
-
-button_settings_sub_net = {
-    .nodraw       = 1,
-    .onpress      = button_settings_sub_net_onpress,
-    .tooltip_text = { .i18nal = STR_NETWORK_SETTINGS },
-},
-
-button_settings_sub_ui = {
-    .nodraw       = 1,
-    .onpress      = button_settings_sub_ui_onpress,
-    .tooltip_text = { .i18nal = STR_USERSETTINGS },
-},
-
-button_settings_sub_av = {
-    .nodraw       = 1,
-    .onpress      = button_settings_sub_av_onpress,
-    .tooltip_text = { .i18nal = STR_AUDIO_VIDEO },
-},
-
 
 button_copyid = {
     .bm = BM_SBUTTON,
