2c2e55d1e02d074e32c45a600e2f7f9b0d24fc7e
endoffile78 <endoffile78@yahoo.com>
2016-11-11 21:18:45 -0600

    Replace native_load_data and native_save_data with native_get_file

--- src/main_native.h
+++ src/main_native.h
@@ -11,9 +11,20 @@
 #include "xlib/main.h"
 #endif
 
+#ifndef FILE_OPTS
+#define FILE_OPTS
+typedef enum UTOX_FILE_OPTS {
+    UTOX_FILE_OPTS_READ   = 1 << 0,
+    UTOX_FILE_OPTS_WRITE  = 1 << 2,
+    UTOX_FILE_OPTS_APPEND = 1 << 3,
+    UTOX_FILE_OPTS_MKDIR  = 1 << 4,
+} UTOX_FILE_OPTS;
+#endif
+
 uint16_t native_video_detect(void);
 bool native_video_init(void *handle);
 void native_video_close(void *handle);
 int native_video_getframe(uint8_t *y, uint8_t *u, uint8_t *v, uint16_t width, uint16_t height);
 bool native_video_startread(void);
 bool native_video_endread(void);
+FILE *native_get_file(char *name, size_t *size, UTOX_FILE_OPTS flag);
