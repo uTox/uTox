cfe770d30025bd54623d657c3e6a00657f744799
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-11-12 16:31:20 -0800

    Fix win/drawing headers and reduce error load in windows

--- cmake/toolchain-win64.cmake
+++ cmake/toolchain-win64.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
 
-set(CMAKE_C_FLAGS "-static-libgcc -static -O3 -s -std=c99 -w -DAL_LIBTYPE_STATIC")
+set(CMAKE_C_FLAGS "-static-libgcc -static -O3 -s -std=c99 -Wp -w -DAL_LIBTYPE_STATIC")
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static-libgcc -static -O3 -s -std=c99 -Wl,-subsystem,windows")
 
 set(INCLUDE_DIRECTORIES SYSTEM /usr/share/mingw-w64/include/)
