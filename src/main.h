c844c24282a3e504acb1dbace10e3801f4ba32c5
Robin Lindén <mail+gitlab@robinlinden.eu>
2016-11-15 02:35:01 +0100

    Clarified comments after code review.

--- src/main.h
+++ src/main.h
@@ -293,7 +293,7 @@ struct utox_mouse {
 uint8_t cursor;
 bool    mdown;
 
-/** Takes a filepath and creates it with permissions 0700 if it doesn't already exist.
+/** Takes a filepath and creates it with permissions 0700 (in posix environments) if it doesn't already exist.
  * 
  * Returns a bool indicating if the path exists or not. */ 
 bool native_create_dir(const char *filepath);
