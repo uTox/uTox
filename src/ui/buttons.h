6a79dfd7c0822372f4351ec540e7cb22029ea9cc
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-11-18 16:44:05 -0800

    make buttons extern

--- src/ui/buttons.h
+++ src/ui/buttons.h
@@ -3,19 +3,30 @@
 
 #include "button.h"
 
-BUTTON button_avatar, button_name, button_status_msg, button_usr_state, button_filter_friends, button_add_new_contact,
-    button_create_group,
+extern BUTTON   button_avatar,
+                button_name,
+                button_status_msg,
+                button_usr_state,
 
-    button_settings, button_settings_sub_profile, button_settings_sub_devices, button_settings_sub_net,
-    button_settings_sub_ui, button_settings_sub_av,
+                button_filter_friends,
+                button_add_new_contact,
 
-    button_add_new_device_to_self,
+                // button_create_group,
 
-    button_copyid, button_change_id_type, button_send_friend_request, button_call_decline, button_call_audio,
-    button_call_video, button_group_audio, button_accept_friend, button_callpreview, button_videopreview,
-    button_send_file, button_send_screenshot, button_chat_send, button_lock_uTox, button_show_password_settings,
+                button_settings,
+                button_settings_sub_profile,
+                button_settings_sub_devices,
+                button_settings_sub_net,
+                button_settings_sub_ui,
+                button_settings_sub_av,
 
-    button_export_chatlog;
+                button_add_new_device_to_self,
+
+                button_copyid, button_change_id_type, button_send_friend_request, button_call_decline, button_call_audio,
+                button_call_video, button_group_audio, button_accept_friend, button_callpreview, button_videopreview,
+                button_send_file, button_send_screenshot, button_chat_send, button_lock_uTox, button_show_password_settings,
+
+                button_export_chatlog;
 
 void button_setcolors_success(BUTTON *b);
 void button_setcolors_danger(BUTTON *b);
