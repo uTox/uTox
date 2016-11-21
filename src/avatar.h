abfff5b2f1da2214a822f73e7240fa89274917cf
endoffile78 <endoffile78@yahoo.com>
2016-11-21 00:00:39 -0600

    Fix avatars

--- src/avatar.h
+++ src/avatar.h
@@ -24,28 +24,29 @@ typedef struct avatar {
 /* whether friend f's avatar is set, where f is a pointer to a friend struct */
 #define friend_has_avatar(f) (f) && (f->avatar.format != UTOX_AVATAR_FORMAT_NONE)
 
-bool avatar_init(char hexid[32], AVATAR *avatar);
+bool avatar_init(char hexid[TOX_PUBLIC_KEY_SIZE * 2], AVATAR *avatar);
 
 /* converts png data given by data to a NATIVE_IMAGE and uses that to populate the avatar struct
  *  avatar is pointer to an avatar struct to store result in. Remains unchanged if function fails.
  *  data is pointer to png data to convert
  *  size is size of data
  *
- *  on success: returns 1
- *  on failure: returns 0
+ *  on success: returns true
+ *  on failure: returns false
  *
  *  notes: fails if given size is larger than UTOX_AVATAR_MAX_DATA_LENGTH or data is not valid PNG data
  */
-int avatar_set(AVATAR *avatar, const uint8_t *data, size_t size);
+bool avatar_set(AVATAR *avatar, const uint8_t *data, size_t size);
 
 /** Helper function for the users avatar */
 bool avatar_set_self(const uint8_t *data, size_t size);
 
 /* unsets an avatar by setting its format to UTOX_AVATAR_FORMAT_NONE and
  * freeing its image */
-void unset_avatar(AVATAR *avatar);
+void avatar_unset(AVATAR *avatar);
 
-bool avatar_unset_self(void);
+/* Helper function to unset the user's avatar */
+void avatar_unset_self(void);
 
 /* sets own avatar based on given png data and saves it to disk if successful
  *  data is png data to set avatar to
@@ -56,7 +57,7 @@ bool avatar_unset_self(void);
  *
  *  notes: fails if size is too large or data is not a valid png file
  */
-int self_set_and_save_avatar(const uint8_t *data, uint32_t size);
+bool self_set_and_save_avatar(const uint8_t *data, uint32_t size);
 
 /* unsets own avatar and removes it from disk */
 bool avatar_remove_self(void);
@@ -73,5 +74,22 @@ bool avatar_on_friend_online(Tox *tox, uint32_t friend_number);
  * If size <=0, we'll unset the avatar, else we'll set and update the friend
  */
 void utox_incoming_avatar(uint32_t friend_number, uint8_t *avatar, size_t size);
+
+/* Saves the avatar for user with hexid
+ *
+ * returns true on success
+ * returns false on failure
+ */
+bool avatar_save(char hexid[TOX_PUBLIC_KEY_SIZE * 2], const uint8_t *data, size_t length);
+
+/* Deletes the avatar for user with hexid
+ *
+ * returns true on success
+ * returns false on failure
+ */
+bool avatar_delete(char hexid[TOX_PUBLIC_KEY_SIZE * 2]);
+
+/* Helper function to intialize the users avatar */
+bool avatar_init_self(void);
 
 #endif
