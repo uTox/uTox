5d11d146262a3faa5ea2147ddcd1b3be98833c22
endoffile78 <endoffile78@yahoo.com>
2016-11-23 20:31:17 -0600

    Change type of nick in edits.c

--- src/ui/edits.c
+++ src/ui/edits.c
@@ -139,7 +139,7 @@ static uint32_t peers_deduplicate(char **dedup, size_t *dedup_size, void **peers
             continue;
         }
 
-        char *nick      = (char *)p->name;
+        uint8_t *nick      = p->name;
         size_t nick_len = p->name_length;
 
         size_t i = 0;
@@ -155,7 +155,7 @@ static uint32_t peers_deduplicate(char **dedup, size_t *dedup_size, void **peers
             }
 
             if (!found) {
-                dedup[count]      = nick;
+                dedup[count]      = (char *)nick;
                 dedup_size[count] = nick_len;
                 count++;
             }
