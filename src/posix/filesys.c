24e6a12f8f02973f92dfd0f49899d30548ec12ea
Robin Lindén <mail+gitlab@robinlinden.eu>
2016-12-11 03:33:38 +0100

    Replace error messages with asserts.

--- src/posix/filesys.c
+++ src/posix/filesys.c
@@ -32,7 +32,7 @@ static void opts_to_sysmode(UTOX_FILE_OPTS opts, char *mode) {
         mode[2] = '+';
     }
 
-    mode[3] = '\0';
+    mode[3] = 0;
 
     return;
 }
@@ -46,13 +46,10 @@ FILE *native_get_file(const uint8_t *name, size_t *size, UTOX_FILE_OPTS opts) {
         snprintf(path, UTOX_FILE_NAME_LENGTH, "%s/.config/tox/", getenv("HOME"));
     }
 
-    if (opts > UTOX_FILE_OPTS_DELETE) {
-        debug_error("NATIVE:\tDon't call native_get_file with UTOX_FILE_OPTS_DELETE in combination with other options.\n");
-        return NULL;
-    } else if (opts & UTOX_FILE_OPTS_WRITE && opts & UTOX_FILE_OPTS_APPEND) {
-        debug_error("NATIVE:\tDon't call native_get_file with UTOX_FILE_OPTS_WRITE in combination with UTOX_FILE_OPTS_APPEND.\n");
-        return NULL;
-    }
+    // native_get_file should never be called with DELETE in combination with other FILE_OPTS.
+    assert(opts <= UTOX_FILE_OPTS_DELETE);
+    // WRITE and APPEND are mutually exclusive. WRITE will serve you a blank file. APPEND will append (duh).
+    assert((opts & UTOX_FILE_OPTS_WRITE && opts & UTOX_FILE_OPTS_APPEND) == false);
 
     if (opts & UTOX_FILE_OPTS_READ || opts & UTOX_FILE_OPTS_MKDIR) {
         if (!native_create_dir(path)) {
@@ -60,11 +57,11 @@ FILE *native_get_file(const uint8_t *name, size_t *size, UTOX_FILE_OPTS opts) {
         }
     }
 
-    if (strlen(path) + strlen(name) >= UTOX_FILE_NAME_LENGTH) {
+    if (strlen((char *)path) + strlen((char *)name) >= UTOX_FILE_NAME_LENGTH) {
         debug("NATIVE:\tLoad directory name too long\n");
         return NULL;
     } else {
-        snprintf(path + strlen(path), UTOX_FILE_NAME_LENGTH - strlen(path), "%s", name);
+        snprintf((char *)path + strlen((char *)path), UTOX_FILE_NAME_LENGTH - strlen((char *)path), "%s", name);
     }
 
     if (opts == UTOX_FILE_OPTS_DELETE) {
