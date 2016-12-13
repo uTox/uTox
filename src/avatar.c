7c6596b22c0b91851d8a73e08a18dcf49db908fb
endoffile78 <endoffile78@yahoo.com>
2016-12-13 07:06:15 -0600

    Add native_move_file to android and made some fixes

--- src/avatar.c
+++ src/avatar.c
@@ -170,21 +170,15 @@ bool avatar_on_friend_online(Tox *tox, uint32_t friend_number) {
     free(avatar_data);
     return true;
 }
-bool avatar_move(uint8_t *source, uint8_t *dest) {
-    char current_name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")] = { 0 };
-    char new_name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")] = { 0 };
 
-#ifdef __WIN32__
-    snprintf(current_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars\\%.*s.png",
+bool avatar_move(const uint8_t *source, const uint8_t *dest) {
+    uint8_t current_name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")] = { 0 };
+    uint8_t new_name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")] = { 0 };
+
+    snprintf((char *)current_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars/%.*s.png",
              TOX_PUBLIC_KEY_SIZE * 2, source);
-    snprintf(new_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars\\%.*s.png",
+    snprintf((char *)new_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars/%.*s.png",
              TOX_PUBLIC_KEY_SIZE * 2, dest);
-#else
-    snprintf(current_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars/%.*s.png",
-             TOX_PUBLIC_KEY_SIZE * 2, source);
-    snprintf(new_name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"), "avatars/%.*s.png",
-             TOX_PUBLIC_KEY_SIZE * 2, dest);
-#endif
 
     return native_move_file(current_name, new_name);
 }
