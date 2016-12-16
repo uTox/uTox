78df8b28e91bbf7095af2fe1fc30fb7d88b74c79
Robin Lindén <mail+gitlab@robinlinden.eu>
2016-12-16 22:24:54 +0100

    Fixing more resumeable file transfer bugs.

--- src/file_transfers.c
+++ src/file_transfers.c
@@ -182,17 +182,19 @@ static bool ft_find_resumeable(FILE_TRANSFER *ft) {
         return false;
     }
 
-    FILE_TRANSFER resume_data;
-    if (size == sizeof(FILE_TRANSFER)) {
-        fread(&resume_data, 1, size, resume_file);
+    if (size != sizeof(FILE_TRANSFER)) {
+        return false;
+    }
 
-        if (!resume_data.in_use
-            || resume_data.in_memory
-            || resume_data.avatar
-            || resume_data.inline_img
-            || !resume_data.resumeable ) {
-            return false;
-        }
+    FILE_TRANSFER resume_data;
+    fread(&resume_data, 1, size, resume_file);
+
+    if (!resume_data.in_use
+        || resume_data.in_memory
+        || resume_data.avatar
+        || resume_data.inline_img
+        || !resume_data.resumeable ) {
+        return false;
     }
 
     memcpy(ft, &resume_data, sizeof(FILE_TRANSFER));
