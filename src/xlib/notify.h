b932916e882bc4971a400dab5d59bfe6d557975f
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-12-23 16:07:12 -0800

    Fix some minor UI glitches in xlib notifications

---
+++ src/xlib/notify.h
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include "window.h"
+#include "drawing.h"
+
+// #include "../main.h"
+#include "../ui.h"
+
+void enddraw_notify(int x, int y, int width, int height) ;
+
+Window native_notify_new(void);
+
+void native_notify_tween(UTOX_WINDOW *target);
