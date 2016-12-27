c11e36ee2eca495204380ed874065554fd53fc7d
Carsten Brandt <mail@cebe.cc>
2016-12-28 00:34:28 +0100

    configure version in one place
    workflow:
    
    - configure version in cmake
    - run `cmake` to generate files
    - commit

--- src/main.h
+++ src/main.h
@@ -23,18 +23,7 @@
  * uTox Versions and header information
  *
  *********************************************************/
-#define TITLE "uTox"
-#define SUB_TITLE "(Alpha)"
-#define RELEASE_TITLE "GOLD MEMBERS"
-#define VERSION "0.11.1"
-#define VER_MAJOR 0
-#define VER_MINOR 11
-#define VER_PATCH 1
-#define UTOX_VERSION_NUMBER (VER_MAJOR * 1000 * 1000 + VER_MINOR * 1000 + VER_PATCH)
-// Defaults
-#define DEFAULT_NAME "uTox User"
-#define DEFAULT_STATUS "Toxing on uTox, from the future!"
-#define DEFAULT_SCALE 11
+#include "branding.h"
 
 /**********************************************************
  *
