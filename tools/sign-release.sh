ca0fdcf2e64cb87bbe6f6b500aab925193594b26
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2017-01-13 00:49:18 -0800

    Add some error checking, and separate the steps

--- tools/sign-release.sh
+++ tools/sign-release.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
-
+# set -e
 TAG=$(git describe --abbrev=0 --tags)
 USER=$(git config --get user.name)
 VERSION=${TAG/v/}
 
-wget "https://github.com/uTox/uTox/archive/$TAG.zip"
-wget "https://github.com/uTox/uTox/archive/$TAG.tar.gz"
+echo "Going to verify and sign releases.\n"
 
-gpg --armor --detach-sign "$TAG.zip"
-gpg --armor --detach-sign "$TAG.tar.gz"
+git diff --exit-code >> /dev/null || (echo "Working Dir not clean, this script won't work." && false)
 
-mv "$TAG.zip.asc" "uTox-$TAG.$USER.zip.asc"
-mv "$TAG.tar.gz.asc" "uTox-$TAG.$USER.tar.gz.asc"
-
+echo "Getting $TAG.zip"
+curl -LOs "https://github.com/uTox/uTox/archive/$TAG.zip"
 unzip -q "$TAG.zip"
 cp -r "uTox-$VERSION"/* .
 rm -r "uTox-$VERSION"
 
+echo "Checking $TAG.zip"
+if git diff --exit-code >> /dev/null; then
+    echo "PASSED $TAG.zip"
+    gpg --armor --detach-sign "$TAG.zip"
+    mv "$TAG.zip.asc" "uTox-$TAG.$USER.zip.asc"
+    rm "$TAG.zip"
+else
+    echo "FAILED $TAG.zip"
+    rm "$TAG.zip"
+fi
+
+
+echo "Getting $TAG.tar.gz"
+curl -LOs "https://github.com/uTox/uTox/archive/$TAG.tar.gz"
 tar xf "$TAG.tar.gz"
 cp -r "uTox-$VERSION"/* .
 rm -r "uTox-$VERSION"
 
-rm "$TAG.zip"
-rm "$TAG.tar.gz"
+echo "Checking $TAG.tar.gz"
+if git diff --exit-code >> /dev/null; then
+    echo "PASSED $TAG.tar.gz"
+    gpg --armor --detach-sign "$TAG.tar.gz"
+    mv "$TAG.tar.gz.asc" "uTox-$TAG.$USER.tar.gz.asc"
+    rm "$TAG.tar.gz"
+else
+    echo "FAILED $TAG.tar.gz"
+    rm "$TAG.tar.gz"
+fi
 
-git diff --exit-code
