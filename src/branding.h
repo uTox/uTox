2253207b0bf635e6a4f1b377e8b428546d3cf072
endoffile78 <endoffile78@yahoo.com>
2017-01-21 09:35:56 -0600

    Version tick

--- src/branding.h
+++ src/branding.h
@@ -8,11 +8,11 @@
 #define TITLE "uTox"
 #define SUB_TITLE "(Alpha)"
 
-#define RELEASE_TITLE "GOLD MEMBERS"
-#define VERSION "0.11.1"
+#define RELEASE_TITLE "Make uTox Great Again"
+#define VERSION "0.12.0"
 #define VER_MAJOR 0
-#define VER_MINOR 11
-#define VER_PATCH 1
+#define VER_MINOR 12
+#define VER_PATCH 0
 #define UTOX_VERSION_NUMBER (VER_MAJOR * 1000 * 1000 + VER_MINOR * 1000 + VER_PATCH)
 
 // Defaults
