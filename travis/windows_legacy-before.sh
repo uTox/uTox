e5e40d7c4002c97edee2c798cc50a107c177ba6b
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2017-02-16 22:47:11 -0800

    Update travis script for win*

--- travis/windows_legacy-before.sh
+++ travis/windows_legacy-before.sh
@@ -99,5 +99,5 @@ fi
 cd ..
 rm -rf openal
 
-mv $CACHE_DIR/usr/lib/libOpenAL32.a $CACHE_DIR/usr/lib/libopenal.a || true
+cp $CACHE_DIR/usr/lib/libOpenAL32.a $CACHE_DIR/usr/lib/libopenal.a || true
 # sudo curl https://cmdline.org/travis/32/shell32.a > $CACHE_DIR/usr/lib/libshell32.a
