893c233cab4760a467c6da0bd16234fabb8cd236
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2017-02-24 11:35:29 -0800

    Make verbose only on failure

--- extra/gitlab/windows-script.sh
+++ extra/gitlab/windows-script.sh
@@ -8,4 +8,4 @@ export CFLAGS="-I$CACHE_DIR/usr/include -I/usr/share/mingw-w64/include/ "
 mkdir build_win
 cd build_win
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-win64.cmake
-make VERBOSE=1
+make || make VERBOSE=1
