8629ce7ebc8cfab0465e6ff7dc907e224eb3a917
Carsten Brandt <mail@cebe.cc>
2017-02-28 01:20:20 +0100

    added -Werror option as ENABLE_WERROR to cmake
    enabled in tests. disabled by default.

--- extra/gitlab/osx-script.sh
+++ extra/gitlab/osx-script.sh
@@ -3,5 +3,5 @@ set -eux
 
 . ./extra/gitlab/env.sh
 
-cmake . -DCMAKE_INCLUDE_PATH=$CACHE_DIR/usr/lib -DENABLE_TESTS=OFF
+cmake . -DCMAKE_INCLUDE_PATH=$CACHE_DIR/usr/lib -DENABLE_TESTS=OFF -DENABLE_WERROR=OFF
 make VERBOSE=1
