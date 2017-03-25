b1e5e9daf69d7d859f546fe4ec24a64e73930c14
endoffile78 <endoffile78@yahoo.com>
2017-03-25 14:23:22 -0500

    Fix tests/

--- tests/test_updater.c
+++ tests/test_updater.c
@@ -1,4 +1,5 @@
 #include "updater.h"
+#include "branding.h"
 
 #include "test.h"
 
@@ -7,7 +8,7 @@
 bool test_new_version(void);
 bool test_no_update(void);
 
-int main() {
+int main(void) {
     int result = 0;
     RUN_TEST(test_no_update);
     RUN_TEST(test_new_version);
@@ -15,25 +16,14 @@ int main() {
 }
 
 bool test_new_version(void) {
-    LOG("Checking for update with an outdated version.");
-
 #define VER_MAJOR 0
 #define VER_MINOR 12
 #define VER_PATCH 0
 #define UTOX_VERSION_NUMBER (VER_MAJOR << 16 | VER_MINOR << 8 | VER_PATCH)
 
-    if (updater_check()) {
-        return true;
-    }
-
-    return false;
+    return updater_check();
 }
 
 bool test_no_update(void) {
-    LOG("Checking for update with the latest version.");
-    if (!updater_check()) {
-        return true;
-    }
-
-    return false;
+    return !updater_check();
 }
