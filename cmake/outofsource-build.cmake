7b0bae75cedc57f91c9545559f3c45d0cc6aca6c
Roland Haeder <roland@mxchange.org>
2017-04-03 20:51:56 +0200

    this prevents in-source builds which would pollute the source tree with build files and maybe overwrite existing files.
    Signed-off-by: Roland Haeder <roland@mxchange.org>

---
+++ cmake/outofsource-build.cmake
@@ -0,0 +1,20 @@
+# This file prevents an in-source-tree build which would mix cloned files and
+# build files.
+#
+# Now you have to do at least:
+#
+# $ mkdir build
+# $ cd build
+# $ cmake ../
+#
+if (${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+	message(FATAL_ERROR "CMake has been ran to create an in-source build. Please create a 'build' path like following and run cmake in there:
+
+$ mkdir build/
+$ cd ./build/
+$ cmake ../
+
+This allows you to have multiple builds (e.g. debug and release) from same source code tree and no build files are being mixed with source files.
+And please cleanup CMakeFiles/ and CMakeCache.txt, this cannot be done from a cmake file:
+https://cmake.org/pipermail/cmake/2014-January/056789.html")
+endif ()
