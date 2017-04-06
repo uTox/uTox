26d5aadca07f8299a2602e79497be24b4219e035
Robin Lindén <dev@robinlinden.eu>
2017-04-06 23:29:42 +0200

    Disable filteraudio in openbsd builds.

--- cmake/toolchain-openbsd.cmake
+++ cmake/toolchain-openbsd.cmake
@@ -1,5 +1,11 @@
+# Required to prevent duplication of flags from this file.
+UNSET(CMAKE_C_FLAGS CACHE)
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I/usr/local/include -L/usr/local/lib -I/usr/X11R6/include -L/usr/X11R6/lib -std=c99" CACHE STRING "" FORCE)
+
 set(ENABLE_TESTS OFF CACHE STRING "" FORCE)
+set(FILTER_AUDIO OFF CACHE STRING "" FORCE)
+
 set(LIBRARIES
     opus
 	openal
