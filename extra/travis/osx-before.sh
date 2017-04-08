dedd0a4216dbd333aae4e5ee314e69baebcc8ee4
endoffile78 <endoffile78@yahoo.com>
2017-04-07 19:40:26 -0500

    Fix variable

--- extra/travis/osx-before.sh
+++ extra/travis/osx-before.sh
@@ -26,7 +26,7 @@ rm -rf libvpx
 
 # install toxcore
 if ! [ -d toxcore ]; then
-  git clone --depth=1 --branch="$TOXCORE_REPO_BRANCH" "$TOXCORE_REOP_URI" toxcore
+  git clone --depth=1 --branch="$TOXCORE_REPO_BRANCH" "$TOXCORE_REPO_URI" toxcore
 fi
 cd toxcore
 git rev-parse HEAD > toxcore.sha
