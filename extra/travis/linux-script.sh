c5a3e441b7c5fb051f5ca081855761288676eeca
Maxim Biro <nurupo.contributions@gmail.com>
2017-04-21 18:57:37 -0400

    test

--- extra/travis/linux-script.sh
+++ extra/travis/linux-script.sh
@@ -2,7 +2,7 @@
 set -e -u -x
 
 . ./extra/travis/env.sh
-
+pkg-config --cflags dbus-1
 cmake . -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/lib" -DENABLE_TESTS=ON -DENABLE_WERROR=ON -DENABLE_DBUS=ON -DENABLE_AUTOUPDATE=ON
 make
 ./run_tests.sh
