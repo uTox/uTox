ba407a07504a19008c6c4ce5e9a205107aaa07d4
Maxim Biro <nurupo.contributions@gmail.com>
2017-04-21 18:39:12 -0400

    Make Travis build with dbus support

--- extra/travis/linux-script.sh
+++ extra/travis/linux-script.sh
@@ -3,6 +3,10 @@ set -e -u -x
 
 . ./extra/travis/env.sh
 
-cmake . -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/lib" -DENABLE_TESTS=ON -DENABLE_WERROR=ON -DENABLE_DBUS=OFF -DENABLE_AUTOUPDATE=ON
+cmake . \
+  -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/lib" \
+  -DENABLE_TESTS=ON -DENABLE_WERROR=ON \
+  -DENABLE_DBUS=ON \
+  -DENABLE_AUTOUPDATE=ON
 make
 ./run_tests.sh
