14d4320de1212c473572ba14695470ec46f61d07
Sebastian Schmidt <publicarray@icloud.com>
2017-05-03 18:02:48 +1000

    fix travis copy changes to gitlab formatting update Info.plist

--- extra/travis/osx-script.sh
+++ extra/travis/osx-script.sh
@@ -3,6 +3,9 @@ set -eux
 
 . ./extra/travis/env.sh
 
-cmake . -DCMAKE_INCLUDE_PATH=$CACHE_DIR/usr/lib -DENABLE_WERROR=OFF
+cmake . \
+  -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/include" \
+  -DCMAKE_LIBRARY_PATH="$CACHE_DIR/usr/lib" \
+  -DENABLE_WERROR=OFF
 make VERBOSE=1
 ./run_tests.sh
