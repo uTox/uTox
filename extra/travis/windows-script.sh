50d2fb6f7e163e5662a991e3a3023307f146c7c3
Sebastian Schmidt <publicarray@icloud.com>
2017-05-13 12:11:28 +1000

    attempt fix travis also copy changes to gitlab

--- extra/travis/windows-script.sh
+++ extra/travis/windows-script.sh
@@ -8,6 +8,8 @@ export CFLAGS="-I$CACHE_DIR/usr/include -I/usr/share/mingw-w64/include/ "
 mkdir build_win
 cd build_win
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-win64.cmake \
+         -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/include" \
+         -DCMAKE_LIBRARY_PATH="$CACHE_DIR/usr/lib" \
          -DENABLE_FILTERAUDIO=OFF \
          -DENABLE_TESTS=OFF \
          -DENABLE_WERROR=OFF \
