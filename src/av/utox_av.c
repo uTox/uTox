d6938916092739d4c24999f34bc0b0431cb3202f
endoffile78 <endoffile78@yahoo.com>
2017-05-31 13:26:11 -0500

    Fix fallthrough warnings

--- src/av/utox_av.c
+++ src/av/utox_av.c
@@ -104,8 +104,9 @@ void utox_av_ctrl_thread(void *UNUSED(args)) {
 
                 case UTOXAV_OUTGOING_CALL_ACCEPTED: {
                     postmessage_audio(UTOXAUDIO_START_FRIEND, msg->param1, msg->param2, NULL);
+                    postmessage_audio(UTOXAUDIO_STOP_RINGTONE, msg->param1, msg->param2, NULL);
                     LOG_NOTE("uToxAV", "Call accepted by friend" );
-                    // intentional fall thorough
+                    break;
                 }
 
                 case UTOXAV_OUTGOING_CALL_REJECTED: {
