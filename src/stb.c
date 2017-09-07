ff0a32b5e412d29415c2b5455573550cf35d3fa8
redmanmale <redmanmale@gmail.com>
2017-09-07 09:34:23 +0300

    Use git submodule for stb

--- src/stb.c
+++ src/stb.c
@@ -1,4 +1,4 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "stb/stb_image_write.h"
--- src/stb.h
+++ src/stb.h
@@ -1,8 +1,8 @@
 #ifndef STB_H
 #define STB_H
 
-#include "stb_image.h"
-#include "stb_image_write.h"
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
 
 // uTox can't find this function unless declared here.
 extern unsigned char *stbi_write_png_to_mem(unsigned char *pixels, int stride_bytes,
