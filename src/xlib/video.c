uTox/uTox

commit 996e2a252042ca20424303e5186f5c98f299d5c2

Clean up header files.

src/xlib/video.c
@@ -11,13 +11,13 @@
 
 #include "../native/time.h"
 
-#include <stdio.h>
-
 #include "../main.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/shm.h>
+#include <sys/stat.h>
 
 #define MAX_VID_WINDOWS 32 // TODO drop this for dynamic allocation
 static Window video_win[MAX_VID_WINDOWS]; // TODO we should allocate this dynamically but this'll work for now
@@ -57,7 +57,7 @@ void video_frame(uint16_t id, uint8_t *img_data, uint16_t width, uint16_t height
     }
 
     XWindowAttributes attrs;
-    XGetWindowAttributes(display, *win, &attrs);
+    XGetWindowAttributes(display, video_win[id], &attrs);
 
     XImage image = {
         .width            = attrs.width,
@@ -73,7 +73,7 @@ void video_frame(uint16_t id, uint8_t *img_data, uint16_t width, uint16_t height
         .green_mask       = 0xFF00,
         .blue_mask        = 0xFF,
         .data             = (char *)img_data
-        };
+    };
 
     /* scale image if needed */
     uint8_t *new_data = malloc(attrs.width * attrs.height * 4);

src/av/video.c
@@ -16,6 +16,7 @@
 #include <tox/toxav.h>
 
 #include <pthread.h>
+#include <stdlib.h>
 #include <vpx/vpx_codec.h>
 #include <vpx/vpx_image.h>
 

langs/i18n_decls.h
@@ -1,8 +1,9 @@
-// i18n_decls.h
 #ifndef I18N_DECLS_H
 #define I18N_DECLS_H
 
-#include "../src/sized_string.h"
+typedef struct sized_string STRING;
+
+#include <stdint.h>
 
 typedef enum {
     LANG_EN,

src/av/audio.c
@@ -20,6 +20,7 @@
 #include "../../langs/i18n_decls.h"
 
 #include <math.h>
+#include <stdlib.h>
 #include <string.h>
 #include <tox/toxav.h>
 

src/avatar.c
@@ -2,10 +2,13 @@
 
 #include "debug.h"
 #include "file_transfers.h"
+#include "filesys.h"
 #include "self.h"
 #include "tox.h"
 
 #include "native/image.h"
+
+#include <stdlib.h>
 
 /* frees the image of an avatar, does nothing if image is NULL */
 static void avatar_free_image(AVATAR *avatar) {

src/debug.h
@@ -1,8 +1,6 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#include <stdlib.h>
-
 #ifndef EXIT_SUCCESS // should be defined in stdlib.h
 #define EXIT_SUCCESS 0 /* Successful exit status. */
 #endif

src/devices.h
@@ -5,7 +5,7 @@
 #ifndef DEVICES_H
 #define DEVICES_H
 
-#include <inttypes.h>
+#include <stdint.h>
 #include <tox/tox.h>
 
 typedef struct UTOX_DEVICE {
@@ -17,7 +17,6 @@ typedef struct UTOX_DEVICE {
 
     //TOX_DEVICE_STATUS status;
     uint8_t status;
-
 } UTOX_DEVICE;
 
 UTOX_DEVICE *devices;

src/file_transfers.c
@@ -15,6 +15,7 @@
 #include "native/thread.h"
 #include "native/time.h"
 
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 

commit 634160aa5ab81fa8e24978d2ba80d2348625e113

fix video preview
revert unrelated change in 996e2a2 which broke video preview

src/xlib/video.c
@@ -57,7 +57,7 @@ void video_frame(uint16_t id, uint8_t *img_data, uint16_t width, uint16_t height
     }
 
     XWindowAttributes attrs;
-    XGetWindowAttributes(display, video_win[id], &attrs);
+    XGetWindowAttributes(display, *win, &attrs);
 
     XImage image = {
         .width            = attrs.width,