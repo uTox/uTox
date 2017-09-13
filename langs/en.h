a55b89218ba9b0d7d5a0c1186b18110581efc4d7
endoffile78 <endoffile78@yahoo.com>
2017-09-13 09:42:09 -0400

    Add comment

--- langs/en.h
+++ langs/en.h
@@ -478,6 +478,11 @@ msgstr("Busy")
 msgid(STATUS_OFFLINE)
 msgstr("Offline")
 
+/*
+ * Leave %.*s. They are variables
+ * The first one is your friend's name
+ * The second one is the state your friend is now in
+ */
 msgid(STATUS_MESSAGE)
 msgstr("uTox %.*s is now %s.")
 
