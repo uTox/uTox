cc5f233342de2e56fed289cff58ca8cb9b37a143
redmanmale <redmanmale@gmail.com>
2017-10-18 21:59:38 +0300

    Drop #ifdef and DISABLE_IDLE option

--- src/windows/main.c
+++ src/windows/main.c
@@ -45,12 +45,10 @@
 #include <io.h>
 #include <libgen.h>
 
+static bool idle = false;
+
 bool flashing = false;
 bool hidden = false;
-
-#if !DISABLE_IDLE_STATUS && !PLATFORM_ANDROID
-static bool idle = false;
-#endif
 
 /** Open system file browser dialog */
 void openfilesend(void) {
@@ -811,7 +809,6 @@ static bool win_init_mutex(HANDLE *mutex, HINSTANCE hInstance, PSTR cmd) {
     return true;
 }
 
-#if !DISABLE_IDLE_STATUS && !PLATFORM_ANDROID
 static void CALLBACK idle_handler(HWND UNUSED(hwnd), UINT UNUSED(message), UINT UNUSED(idTimer), DWORD dwTime)
 {
     if (!settings.idle_status) {
@@ -845,7 +842,6 @@ static void CALLBACK idle_handler(HWND UNUSED(hwnd), UINT UNUSED(message), UINT 
         idle = false;
     }
 }
-#endif
 
 /** client main()
  *
@@ -976,9 +972,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE UNUSED(hPrevInstance), PSTR cm
         }
     }
 
-    #if !DISABLE_IDLE_STATUS && !PLATFORM_ANDROID
     SetTimer(main_window.window, rand(), idle_check_period * 1000, (TIMERPROC)idle_handler);
-    #endif
 
     if (settings.start_in_tray) {
         ShowWindow(main_window.window, SW_HIDE);
