c63a3c7b5579a7f3d3bdefcc4c03590c52ba761a
redmanmale <redmanmale@gmail.com>
2017-10-30 21:28:08 +0300

    Implement export chat log for GTK

--- src/xlib/gtk.h
+++ src/xlib/gtk.h
@@ -2,10 +2,15 @@
 #define UTOX_GTK_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct file_transfer FILE_TRANSFER;
 typedef struct msg_header MSG_HEADER;
-typedef struct file_transfer FILE_TRANSFER;
+
+typedef struct {
+	uint32_t chat_number;
+	bool 	 is_groupchat;
+} CHAT;
 
 typedef struct {
     char *name;
@@ -21,7 +26,7 @@ void ugtk_native_select_dir_ft(uint32_t fid, FILE_TRANSFER *file);
 
 void ugtk_file_save_inline(MSG_HEADER *msg);
 
-void ugtk_save_chatlog(uint32_t friend_number);
+void ugtk_save_chatlog(CHAT *chat);
 
 /**
  * @brief Save passed image to selected file.
