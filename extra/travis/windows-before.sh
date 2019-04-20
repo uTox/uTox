b51ca8481b2676d2d0e69381bfb74301f5a56d6a
Gregory Mullen <greg@grayhatter.com>
2019-04-20 13:54:58 -0700

    try to upgrade travis

--- extra/travis/windows-before.sh
+++ extra/travis/windows-before.sh
@@ -78,4 +78,7 @@ x86_64-w64-mingw32-ranlib "$CACHE_DIR/usr/lib/libfilteraudio.a"
 unset CC
 
 cp "$CACHE_DIR/usr/lib/libOpenAL32.a" "$CACHE_DIR/usr/lib/libopenal.a" || true
-curl https://cmdline.org/libshell32.a > "$CACHE_DIR/usr/lib/libshell32.a"
+
+if ! [ -f "$CACHE_DIR/usr/lib/libshell32.a"]; then
+  curl https://cmdline.org/libshell32.a > "$CACHE_DIR/usr/lib/libshell32.a"
+fi
