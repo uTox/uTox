uTox/uTox

commit 8ac11d57c9d6c7304d27e84433d03571d5bbf15b

Fix more snprintf()s
Closes #1282

src/layout/settings.c
@@ -39,10 +39,15 @@ static void draw_settings_header(int x, int y, int w, int UNUSED(height)) {
 #ifdef GIT_VERSION
     setfont(FONT_TEXT);
     char ver_string[64];
-    int  count = snprintf(ver_string, 64, "Toxcore v%u.%u.%u",
-                            tox_version_major(), tox_version_minor(), tox_version_patch());
-    drawtextwidth_right(x + w - textwidth(ver_string, count) , textwidth(ver_string, count), SCALE(10),
-                        ver_string, count);
+    int  ver_string_len;
+
+    snprintf(ver_string, sizeof(ver_string), "Toxcore v%u.%u.%u",
+             tox_version_major(), tox_version_minor(), tox_version_patch());
+    ver_string_len = strnlen(ver_string_len, sizeof(ver_string) - 1);
+
+    drawtextwidth_right(x + w - textwidth(ver_string, ver_string_len),
+                        textwidth(ver_string, ver_string_len), SCALE(10),
+                        ver_string, ver_string_len);
 
     setfont(FONT_SELF_NAME); // x adjustment depends on the font type being set first
     x += SCALE(25) + UTOX_STR_WIDTH(UTOX_SETTINGS);
@@ -171,9 +176,12 @@ static void draw_settings_text_devices(int x, int y, int UNUSED(w), int UNUSED(h
     drawstr(x + SCALE(10), y + SCALE(60), DEVICES_NUMBER);
 
     char   str[10];
-    size_t strlen = snprintf(str, 10, "%zu", self.device_list_count);
+    size_t str_len;
 
-    drawtext(x + SCALE(10), y + SCALE(75), str, strlen);
+    snprintf(str, sizeof(str), "%zu", self.device_list_count);
+    str_len = strnlen(str, sizeof(str) - 1);
+
+    drawtext(x + SCALE(10), y + SCALE(75), str, str_len);
 }
 
 static void draw_settings_text_password(int x, int y, int UNUSED(w), int UNUSED(h)) {

src/av/utox_av.c
@@ -17,6 +17,7 @@
 #include "../native/thread.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 bool utox_av_ctrl_init = false;
 
@@ -145,11 +146,15 @@ void utox_av_ctrl_thread(void *UNUSED(args)) {
 
                     if (f && f->call_started != 0) {
                         char notice_msg[64];
+                        int  notice_msg_len;
                         int duration = difftime(time(NULL), f->call_started);
-                        int length = snprintf(notice_msg, 64, "%s: %02u:%02u:%02u",
-                                S(CALL_ENDED), duration / 3600, (duration / 60) % 60, duration % 60);
-                        if (length < 64) {
-                            message_add_type_notice(&f->msg, notice_msg, length, true);
+
+                        snprintf(notice_msg, sizeof(notice_msg), "%s: %02u:%02u:%02u",
+                                 S(CALL_ENDED), duration / 3600, (duration / 60) % 60, duration % 60);
+                        notice_msg_len = strnlen(notice_msg, sizeof(notice_msg) - 1);
+
+                        if (notice_msg_len < 64) {
+                            message_add_type_notice(&f->msg, notice_msg, notice_msg_len, true);
                         }
                         f->call_started = 0;
                     }

src/avatar.c
@@ -9,6 +9,7 @@
 #include "native/image.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 /* frees the image of an avatar, does nothing if image is NULL */
 static void avatar_free_image(AVATAR *avatar) {
@@ -74,9 +75,10 @@ static uint8_t *load_img_data(char hexid[TOX_PUBLIC_KEY_SIZE * 2], size_t *out_s
 
 bool avatar_delete(char hexid[TOX_PUBLIC_KEY_SIZE * 2]) {
     char name[sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png")] = { 0 };
+    int name_len;
 
-    int name_len = snprintf(name, sizeof("avatars/") + TOX_PUBLIC_KEY_SIZE * 2 + sizeof(".png"),
-                            "avatars/%.*s.png", TOX_PUBLIC_KEY_SIZE * 2, hexid);
+    snprintf(name, sizeof(name), "avatars/%.*s.png", TOX_PUBLIC_KEY_SIZE * 2, hexid);
+    name_len = strnlen(name, sizeof(name) - 1);
 
     return utox_remove_file((uint8_t *)name, name_len);
 }

src/friend.c
@@ -311,16 +311,19 @@ void utox_friend_list_init(Tox *tox) {
 
 void friend_setname(FRIEND *f, uint8_t *name, size_t length) {
     if (f->name && f->name_length) {
-        size_t size = sizeof(" is now known as ") + f->name_length + length;
+        char *p;
+        size_t p_size = sizeof(" is now known as ") + f->name_length + length;
+        size_t p_len;
 
-        char *p = calloc(1, size);
+        p = calloc(1, p_size);
         if (!p) {
-            LOG_FATAL_ERR(EXIT_MALLOC, "Friend", "Could not alloc space for name change message (%uB)", size);
+            LOG_FATAL_ERR(EXIT_MALLOC, "Friend", "Could not alloc space for name change message (%uB)", p_size);
         }
-        size = snprintf(p, size, "%.*s is now known as %.*s", (int)f->name_length, f->name, (int)length, name);
+        snprintf(p, p_size, "%.*s is now known as %.*s", (int)f->name_length, f->name, (int)length, name);
+        p_len = strnlen(p, p_size - 1);
 
         if (length != f->name_length || memcmp(f->name, name, (length < f->name_length ? length : f->name_length))) {
-            message_add_type_notice(&f->msg, p, size, 1);
+            message_add_type_notice(&f->msg, p, p_len, 1);
         }
 
         free(f->name);
@@ -410,10 +413,12 @@ void friend_recvimage(FRIEND *f, NATIVE_IMAGE *native_image, uint16_t width, uin
 }
 
 void friend_notify_msg(FRIEND *f, const char *msg, size_t msg_length) {
-    char title[UTOX_FRIEND_NAME_LENGTH(f) + 25];
+    char title[sizeof("uTox new message from ") + UTOX_FRIEND_NAME_LENGTH(f)];
+    size_t title_length;
 
-    size_t title_length = snprintf((char *)title, UTOX_FRIEND_NAME_LENGTH(f) + 25, "uTox new message from %.*s",
-                                   (int)UTOX_FRIEND_NAME_LENGTH(f), UTOX_FRIEND_NAME(f));
+    snprintf((char *)title, sizeof(title), "uTox new message from %.*s",
+             (int)UTOX_FRIEND_NAME_LENGTH(f), UTOX_FRIEND_NAME(f));
+    title_length = strnlen(title, sizeof(title) - 1);
 
     postmessage_utox(FRIEND_MESSAGE, f->number, 0, NULL);
     notify(title, title_length, msg, msg_length, f, 0);
@@ -577,10 +582,12 @@ void friend_notify_status(FRIEND *f, const uint8_t *msg, size_t msg_length, char
         return;
     }
 
-    const int size = UTOX_FRIEND_NAME_LENGTH(f) + SLEN(STATUS_MESSAGE) + strlen(state);
-    char title[size];
-    size_t  title_length = snprintf(title, size, S(STATUS_MESSAGE),
-                                   (int)UTOX_FRIEND_NAME_LENGTH(f), UTOX_FRIEND_NAME(f), state);
+    char title[UTOX_FRIEND_NAME_LENGTH(f) + SLEN(STATUS_MESSAGE) + strlen(state)];
+    size_t title_length;
+
+    snprintf(title, sizeof(title), S(STATUS_MESSAGE),
+             (int)UTOX_FRIEND_NAME_LENGTH(f), UTOX_FRIEND_NAME(f), state);
+    title_length = strnlen(title, sizeof(title) - 1);
 
     notify(title, title_length, (char *)msg, msg_length, f, 0);
 

src/groups.c
@@ -230,8 +230,8 @@ void group_peer_name_change(GROUPCHAT *g, uint32_t peer_id, const uint8_t *name,
         char msg[TOX_MAX_NAME_LENGTH];
 
         memcpy(old, peer->name, peer->name_length);
-        size_t size = snprintf(msg, TOX_MAX_NAME_LENGTH, "<- has changed their name from %.*s",
-                               peer->name_length, old);
+        snprintf(msg, sizeof(msg), "<- has changed their name from %.*s",
+                 peer->name_length, old);
 
         GROUP_PEER *new_peer = realloc(peer, sizeof(GROUP_PEER) + sizeof(char) * length);
         if (!new_peer) {
@@ -245,7 +245,8 @@ void group_peer_name_change(GROUPCHAT *g, uint32_t peer_id, const uint8_t *name,
         g->peer[peer_id] = peer;
 
         pthread_mutex_unlock(&messages_lock);
-        group_add_message(g, peer_id, (uint8_t *)msg, size, MSG_TYPE_NOTICE);
+        size_t msg_length = strnlen(msg, sizeof(msg) - 1);
+        group_add_message(g, peer_id, (uint8_t *)msg, msg_length, MSG_TYPE_NOTICE);
         return;
     }
 
@@ -349,9 +350,10 @@ void group_notify_msg(GROUPCHAT *g, const char *msg, size_t msg_length) {
     }
 
     char title[g->name_length + 25];
+    size_t title_length;
 
-    size_t title_length = snprintf(title, g->name_length + 25, "uTox new message in %.*s", g->name_length, g->name);
-
+    snprintf(title, sizeof(title), "uTox new message in %.*s", g->name_length, g->name);
+    title_length = strnlen(title, sizeof(title) - 1);
     notify(title, title_length, msg, msg_length, g, 1);
 
     if (flist_get_groupchat() != g) {

src/layout/group.c
@@ -57,9 +57,13 @@ static void draw_group(int x, int UNUSED(y), int UNUSED(w), int UNUSED(height)) 
 
         if (peer && peer->name_length) {
             char buf[TOX_MAX_NAME_LENGTH];
-            int  text_length = snprintf((char *)buf, TOX_MAX_NAME_LENGTH, "%.*s, ", (int)peer->name_length, peer->name);
+            int  buf_len;
 
-            unsigned w = textwidth(buf, text_length);
+            snprintf((char *)buf, sizeof(buf), "%.*s, ",
+                     (int)peer->name_length, peer->name);
+            buf_len = strnlen(buf, sizeof(buf) - 1);
+
+            unsigned w = textwidth(buf, buf_len);
             if (peer->name_color) {
                 setcolor(peer->name_color);
             } else {
@@ -76,7 +80,7 @@ static void draw_group(int x, int UNUSED(y), int UNUSED(w), int UNUSED(height)) 
                 }
             }
 
-            drawtext(k, SCALE(pos_y * 2), buf, text_length);
+            drawtext(k, SCALE(pos_y * 2), buf, buf_len);
 
             k += w;
         }

src/messages.c
@@ -828,12 +828,12 @@ static void messages_draw_filetransfer(MESSAGES *m, MSG_FILE *file, uint32_t i, 
         file_percent = 1.0;
     }
 
-    int max = file->name_length + 128;
-    char ft_text[max];
-    char *text = ft_text;
+    char ft_text[file->name_length + 128];
+    size_t ft_text_length;
 
-    text += snprintf(text, max, "%.*s ", (int)file->name_length, file->name);
-    text += sprint_humanread_bytes(text, text - ft_text, file->size);
+    snprintf(ft_text, sizeof(ft_text), "%.*s ", (int)file->name_length, file->name);
+    ft_text_length = strnlen(ft_text, sizeof(ft_text) - 1);
+    ft_text_length += sprint_humanread_bytes(ft_text, sizeof(ft_text) - ft_text_length, file->size);
 
     setfont(FONT_MISC);
     setcolor(COLOR_BKGRND_MAIN);
@@ -926,11 +926,13 @@ static void messages_draw_filetransfer(MESSAGES *m, MSG_FILE *file, uint32_t i, 
             DRAW_FT_PAUSE_BTN();
 
             char speed[32] = {0};
-            char *p = speed + sprint_humanread_bytes(speed, 32, file->speed);
-            p += snprintf(p, speed - p, "/s %lus",
-                               file->speed ? (file->size - file->progress) / file->speed : 0);
-            DRAW_FT_TEXT_RIGHT(speed, p - speed);
+            size_t speed_len;
+            speed_len = sprint_humanread_bytes(speed, sizeof(speed), file->speed);
+            snprintf(speed + speed_len, sizeof(speed) - speed_len, "/s %lus",
+                     file->speed ? (file->size - file->progress) / file->speed : 0);
+            speed_len = strnlen(speed, sizeof(speed) - 1);
 
+            DRAW_FT_TEXT_RIGHT(speed, speed_len);
             DRAW_FT_PROG(COLOR_BTN_INPROGRESS_FORGRND);
             break;
         }
@@ -960,7 +962,7 @@ static void messages_draw_filetransfer(MESSAGES *m, MSG_FILE *file, uint32_t i, 
     }
 
     setfont(FONT_TEXT);
-    drawtextrange(dx + SCALE(10), wbound - SCALE(10), y + SCALE(6), ft_text, text - ft_text);
+    drawtextrange(dx + SCALE(10), wbound - SCALE(10), y + SCALE(6), ft_text, ft_text_length);
 }
 
 /* This is a bit hacky, and likely would benifit from being moved to a whole new section including seperating

src/text.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* returns the length of the string written to `dest`
+ */
 int sprint_humanread_bytes(char *dest, unsigned int size, uint64_t bytes) {
     char * str[]  = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB" };
     int    max_id = COUNTOF(str) - 1;

commit b89caedfa25f2b314bb7f4f29ddfdc63dc56d649

Fix a segfault in the debug build preventing utox to start
Introduced by: 8ac11d57c9d6c7304d27e84433d03571d5bbf15b Fix more snprintf()s

This bug was overlooked, because it was tested from the release build (GIT_VERSION was not set), so the faulty code was never compiled.
Release builds were not affected.

src/layout/settings.c
@@ -43,7 +43,7 @@ static void draw_settings_header(int x, int y, int w, int UNUSED(height)) {
 
     snprintf(ver_string, sizeof(ver_string), "Toxcore v%u.%u.%u",
              tox_version_major(), tox_version_minor(), tox_version_patch());
-    ver_string_len = strnlen(ver_string_len, sizeof(ver_string) - 1);
+    ver_string_len = strnlen(ver_string, sizeof(ver_string) - 1);
 
     drawtextwidth_right(x + w - textwidth(ver_string, ver_string_len),
                         textwidth(ver_string, ver_string_len), SCALE(10),