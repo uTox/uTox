777ab94c5693007c41db8d629ed620bf6bed3abc
avoidr <avoidr@firemail.cc>
2019-10-03 23:39:06 +0200

    tools/update-changelog.sh: POSIX compatibility
    And simplify to let the shell output its own error message.

--- tools/update-changelog.sh
+++ tools/update-changelog.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 TOKEN=$(git config --get user.token)
 
-if [[ -z "$TOKEN" ]]; then
+if [ -z "$TOKEN" ]; then
     echo "Please add your github token to user.token"
     echo "Run git config --local user.token [token]"
-    exit 1
-fi
-
-if [[ $(which github_changelog_generator) < /dev/null ]]; then
-    echo "github_changelog_generator can not be found."
     exit 1
 fi
 
