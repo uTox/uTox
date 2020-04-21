b370fc9e52f323a66c00a37a364286e788da9ca2
redmanmale <redmanmale@gmail.com>
2020-04-21 22:19:06 +0300

    Rename (and fix usages) SETTINGS fields to match UTOXSAVE struct to be able to load it from config directly

--- src/av/filter_audio.c
+++ src/av/filter_audio.c
@@ -14,20 +14,20 @@ Filter_Audio *f_a = NULL;
 
 bool filter_audio_check(void) {
 #ifdef AUDIO_FILTERING
-    if (!f_a && settings.audiofilter_enabled) {
+    if (!f_a && settings.audio_filtering_enabled) {
         f_a = new_filter_audio(UTOX_DEFAULT_SAMPLE_RATE_A);
         if (!f_a) {
             LOG_INFO("Filter Audio", "filter audio failed" );
             return false;
         }
         LOG_INFO("Filter Audio", "filter audio on" );
-    } else if (f_a && !settings.audiofilter_enabled) {
+    } else if (f_a && !settings.audio_filtering_enabled) {
         kill_filter_audio(f_a);
         f_a = NULL;
         LOG_INFO("Filter Audio", "filter audio off" );
         return false;
     }
-    return settings.audiofilter_enabled;
+    return settings.audio_filtering_enabled;
 #else
     return false;
 #endif
