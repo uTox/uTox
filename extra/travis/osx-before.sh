uTox/uTox

commit 05ccede2eee2a71cca5cc6ca21de0d80e7a19c2b

Remove libsodium dependency
After commit 3d1b1df7 ("Remove auto-updater"), no code uses libsodium
directly anymore.

However, systems that link statically (Windows, Android) require the
library for the object archive file to link against. For dynamic
linking, the libsodium dependency is transitive by toxcore.

extra/travis/osx-before.sh
@@ -9,7 +9,6 @@ brew update
 brew install yasm
 brew install check
 
-. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 
 # install libvpx, needed for video encoding/decoding

extra/gitlab/osx-before.sh
@@ -5,7 +5,6 @@ brew install yasm
 
 . ./extra/gitlab/env.sh
 
-. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 
 # install libvpx, needed for video encoding/decoding

extra/gitlab/linux-before.sh
@@ -3,7 +3,6 @@ set -eux
 
 . ./extra/gitlab/env.sh
 
-. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 
 # install libvpx, needed for video encoding/decoding

extra/gitlab/linux-muslc-before.sh
@@ -5,7 +5,6 @@ set -eux
 
 export TARGET_TRGT="--enable-pic "
 export CFLAGS="${CFLAGS} -fPIC"
-. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 . ./extra/common/build_vpx.sh
 

extra/gitlab/osx-docker.sh
@@ -27,28 +27,6 @@ export LDFLAGS="${LDFLAGS} -L${CACHE_DIR}/usr/lib "
 export LDFLAGS="${LDFLAGS} -isysroot /usr/osxcross/bin/../SDK/MacOSX10.10.sdk "
 export LDFLAGS="${LDFLAGS} -mmacosx-version-min=10.10 -m64 -arch x86_64"
 
-# install libsodium, needed for crypto
-if ! [ -d libsodium ]; then
-  git clone --depth=1 --branch=stable https://github.com/jedisct1/libsodium.git
-fi
-cd libsodium
-git rev-parse HEAD > libsodium.sha
-if ! ([ -f "$CACHE_DIR/libsodium.sha" ] && diff "$CACHE_DIR/libsodium.sha" libsodium.sha); then
-  ./autogen.sh
-  ./configure $TARGET_HOST \
-              --prefix="$CACHE_DIR/usr" \
-              --quiet
-  # libtool is broken when it comes to spaces in vars, so we have to neuter them
-  # I live in backslash escapement hell...
-  # This is also why we can't use ../common/*
-  find . -type f -exec sed -i 's/libsodium\\\\ 1.0.12/libnacl-str/g' {} +
-  make -j8
-  make install
-  mv libsodium.sha "$CACHE_DIR/libsodium.sha"
-fi
-cd ..
-# rm -rf libsodium
-
 . ./extra/common/build_opus.sh
 . ./extra/common/build_vpx.sh
 

extra/travis/linux-before.sh
@@ -4,7 +4,6 @@ set -eux
 
 . ./extra/travis/env.sh
 
-. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 
 # install libvpx, needed for video encoding/decoding

extra/travis/osx-docker.sh
@@ -9,25 +9,6 @@ export LDFLAGS="-L/workdir/cache/usr/lib -isysroot /usr/osxcross/bin/../SDK/MacO
 
 # brew install yasm
 
-# install libsodium, needed for crypto
-if ! [ -d libsodium ]; then
-  git clone --depth=1 --branch=stable https://github.com/jedisct1/libsodium.git
-fi
-cd libsodium
-git rev-parse HEAD > libsodium.sha
-if ! ([ -f "$CACHE_DIR/libsodium.sha" ] && diff "$CACHE_DIR/libsodium.sha" libsodium.sha); then
-  ./autogen.sh
-  ./configure --prefix="$CACHE_DIR/usr" --host="x86_64-apple-darwin14" --quiet
-  # libtool is broken when it comes to spaces in vars, so we have to neuter them
-  # I live in backslash escapement hell...
-  find . -type f -exec sed -i 's/libsodium\\\\ 1.0.11/libnacl-str/g' {} +
-  make -j8
-  make install
-  mv libsodium.sha "$CACHE_DIR/libsodium.sha"
-fi
-cd ..
-# rm -rf libsodium
-
 # install libopus, needed for audio encoding/decoding
 if ! [ -f $CACHE_DIR/usr/lib/pkgconfig/opus.pc ]; then
   curl https://ftp.osuosl.org/pub/xiph/releases/opus/opus-1.2.1.tar.gz -o opus.tar.gz

commit 64c7938ba3df28620d235936215f3bc176f995c0

Require libsodium only when linking statically
Fixes: 05ccede2 ("Remove libsodium dependency")

CMakeLists.txt
@@ -271,9 +271,11 @@ find_package(libtox REQUIRED)
 include_directories(${LIBTOX_INCLUDE_DIRS})
 set(LIBRARIES ${LIBRARIES} ${LIBTOX_LIBRARIES})
 
-find_package(libsodium REQUIRED)
-include_directories(${LIBSODIUM_INCLUDE_DIRS})
-set(LIBRARIES ${LIBRARIES} ${LIBSODIUM_LIBRARIES})
+if(STATIC_ALL OR STATIC_TOXCORE OR STATIC_SODIUM OR APPLE)
+    find_package(libsodium REQUIRED)
+    include_directories(${LIBSODIUM_INCLUDE_DIRS})
+    set(LIBRARIES ${LIBRARIES} ${LIBSODIUM_LIBRARIES})
+endif()
 
 find_package(libvpx REQUIRED)
 include_directories(${LIBVPX_INCLUDE_DIRS})

extra/travis/osx-before.sh
@@ -9,6 +9,7 @@ brew update
 brew install yasm
 brew install check
 
+. ./extra/common/build_nacl.sh
 . ./extra/common/build_opus.sh
 
 # install libvpx, needed for video encoding/decoding