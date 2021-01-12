98aa3095cb2016d02e28fb88555f7628779309e7
Pablo <siburec@protonmail.com>
2021-01-12 16:59:11 +0100

    Fix heap-use-after-free when creating new group chats
    The error is caused because after the memory is reallocated to store a new
    element, the selected_item was still pointing to the old position.

--- src/flist.c
+++ src/flist.c
@@ -283,10 +283,23 @@ void flist_update_shown_list(void) {
 
 /* returns address of item at current index and appends the group create entry */
 static ITEM *newitem(void) {
+    int64_t old_selected_index = -1;
+
+    for (int64_t i = 0; i < itemcount; ++i) {
+        if (selected_item == &(item[i])) {
+            old_selected_index = i;
+            break;
+        }
+    }
+
     item       = realloc(item, (itemcount + 1) * sizeof(ITEM));
     shown_list = realloc(shown_list, (itemcount + 1) * sizeof(uint32_t));
     if (!item || !shown_list) {
         LOG_FATAL_ERR(EXIT_MALLOC, "flist", "Could not allocate memory for friend list.");
+    }
+
+    if (old_selected_index != -1) {
+        selected_item = &(item[old_selected_index]);
     }
 
     unsigned int index = itemcount - 1;
