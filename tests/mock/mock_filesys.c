7534298147c0bab1725e91c86c46598ad1e922f9
redmanmale <redmanmale@gmail.com>
2026-08-10 23:12:24 +0300

    Move edits to fix Cygwin tests to the test files

--- tests/mock/mock_filesys.c
+++ tests/mock/mock_filesys.c
@@ -1,8 +1,136 @@
 #include "../../src/filesys.h"
 #include "../../src/native/filesys.h"
 
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Chatlog unit tests pull filesys through this mock.
+ * On Windows/MinGW do not compile posix/filesys.c (mkdir(2) / O_BINARY);
+ * provide only the natives chatlog actually uses. Real UNIX builds keep posix.
+ */
+#ifdef _WIN32
+
+#include <direct.h>
+
+#include "../../src/debug.h"
+#include "../../src/settings.h"
+
+bool native_create_dir(const uint8_t *filepath) {
+    const int status = _mkdir((const char *)filepath);
+    if (status == 0 || errno == EEXIST) {
+        return true;
+    }
+    LOG_WARN("Filesys", "Unable to create directory %s. Error: %d", filepath, errno);
+    return false;
+}
+
+static void opts_to_sysmode(UTOX_FILE_OPTS opts, char *mode) {
+    if (opts & UTOX_FILE_OPTS_READ) {
+        mode[0] = 'r';
+    } else if (opts & UTOX_FILE_OPTS_APPEND) {
+        mode[0] = 'a';
+    } else if (opts & UTOX_FILE_OPTS_WRITE) {
+        mode[0] = 'w';
+    }
+
+    mode[1] = 'b';
+
+    if ((opts & (UTOX_FILE_OPTS_WRITE | UTOX_FILE_OPTS_APPEND)) && (opts & UTOX_FILE_OPTS_READ)) {
+        mode[2] = '+';
+    }
+
+    mode[3] = 0;
+}
+
+FILE *native_get_file_simple(const char *path, UTOX_FILE_OPTS opts) {
+    char mode[4] = { 0 };
+    opts_to_sysmode(opts, mode);
+
+    FILE *fp = fopen(path, mode);
+    if (!fp && (opts & UTOX_FILE_OPTS_READ) && (opts & UTOX_FILE_OPTS_WRITE)) {
+        /* Create then reopen with the requested mode (binary via 'b'). */
+        fp = fopen(path, "w+b");
+        if (fp) {
+            fclose(fp);
+            fp = fopen(path, mode);
+        }
+    }
+
+    return fp;
+}
+
+FILE *native_get_file(const uint8_t *name, size_t *size, UTOX_FILE_OPTS opts, bool portable_mode) {
+    uint8_t path[UTOX_FILE_NAME_LENGTH] = { 0 };
+
+    if (portable_mode) {
+        snprintf((char *)path, UTOX_FILE_NAME_LENGTH, "./tox/");
+    } else {
+        snprintf((char *)path, UTOX_FILE_NAME_LENGTH, "%s/.config/tox/", getenv("HOME"));
+    }
+
+    assert(opts <= UTOX_FILE_OPTS_DELETE);
+    assert((opts & UTOX_FILE_OPTS_WRITE && opts & UTOX_FILE_OPTS_APPEND) == false);
+
+    if (opts & UTOX_FILE_OPTS_WRITE || opts & UTOX_FILE_OPTS_MKDIR) {
+        if (!native_create_dir(path)) {
+            return NULL;
+        }
+    }
+
+    if (strlen((char *)path) + strlen((char *)name) >= UTOX_FILE_NAME_LENGTH) {
+        LOG_ERR("Filesys", "Load directory name too long");
+        return NULL;
+    }
+
+    snprintf((char *)path + strlen((char *)path), UTOX_FILE_NAME_LENGTH - strlen((char *)path), "%s", name);
+
+    if (opts == UTOX_FILE_OPTS_DELETE) {
+        remove((char *)path);
+        return NULL;
+    }
+
+    if (opts & UTOX_FILE_OPTS_MKDIR) {
+        uint8_t push;
+        uint8_t *p = path + strlen((char *)path);
+        while (*--p != '/') {
+        }
+        push = *++p;
+        *p   = 0;
+        native_create_dir(path);
+        *p = push;
+    }
+
+    FILE *fp = native_get_file_simple((char *)path, opts);
+    if (fp == NULL) {
+        return NULL;
+    }
+
+    if (size != NULL) {
+        fseek(fp, 0, SEEK_END);
+        *size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+    }
+
+    return fp;
+}
+
+FILE *utox_get_file(const char *name, size_t *size, UTOX_FILE_OPTS opts) {
+    return native_get_file((uint8_t *)name, size, opts, settings.portable_mode);
+}
+
+bool utox_remove_file(const uint8_t *full_name, size_t length) {
+    return native_remove_file(full_name, length, settings.portable_mode);
+}
+
+#else /* !_WIN32 */
+
 #include "../../src/filesys.c"
 #include "../../src/posix/filesys.c"
+
+#endif /* _WIN32 */
 
 // TODO copied from xlib/filesys.c might be possible to keep this DRY at some point
 bool native_remove_file(const uint8_t *name, size_t length, bool portable_mode) {
@@ -16,19 +144,18 @@ bool native_remove_file(const uint8_t *name, size_t length, bool portable_mode) 
     }
 
     if (strlen((const char *)path) + length >= UTOX_FILE_NAME_LENGTH) {
-        LOG_DEBUG("Filesys", "File/directory name too long, unable to remove" );
-        return false;
+        LOG_DEBUG("Filesys", "File/directory name too long, unable to remove");
     } else {
         snprintf((char *)path + strlen((const char *)path), UTOX_FILE_NAME_LENGTH - strlen((const char *)path), "%.*s",
                  (int)length, (char *)name);
     }
 
     if (remove((const char *)path)) {
-        LOG_ERR("NATIVE", "Unable to delete file!\n\t\t%s" , path);
+        LOG_ERR("NATIVE", "Unable to delete file!\n\t\t%s", path);
         return false;
-    } else {
-        LOG_INFO("NATIVE", "File deleted!" );
-        LOG_DEBUG("Filesys", "\t%s" , path);
     }
+
+    LOG_INFO("NATIVE", "File deleted!");
+    LOG_DEBUG("Filesys", "\t%s", path);
     return true;
 }
